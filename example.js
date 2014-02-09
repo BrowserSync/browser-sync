04a264d84a0b9f7190aa271418f945c07b946d7b
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-09 02:31:38 +0000

    Add contribution message to control panel

--- example.js
+++ example.js
@@ -1,9 +1,22 @@
 var browserSync = require("./lib/index");
 
-var bs = browserSync.init("test/fixtures/**/*.css", {
+//var bs = browserSync.init("test/fixtures/**/*.css", {
+//    server: {
+//        baseDir: "test/fixtures"
+//    },
+//    reloadDelay: 2000
+//});
+
+var bs = browserSync.init(null, {
     server: {
         baseDir: "test/fixtures"
-    },
-    reloadDelay: 2000
+    }
+}).on("init", function (api) {
+
+}).on("file:reload", function (file) {
+    console.log("FILE INJECTED: " + file.assetFileName);
 });
 
+setInterval(function () {
+    bs.emit("file:changed", {path: "style.css"});
+}, 5000);
