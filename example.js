3ad54a49e4af88ecfe06bc782bf057a68339998a
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-22 16:55:14 +0000

    add respmodifier

--- example.js
+++ example.js
@@ -2,13 +2,13 @@ var browserSync = require("./lib/index");
 
 var files = ["test/fixtures/**/*.css", "test/fixtures/**/*.html"];
 var bs = browserSync.init(files, {
-//    server: {
-//        baseDir: "test/fixtures"
-//    },
-    proxy: {
-        host: "172.22.22.22",
-        port: 80
+    server: {
+        baseDir: "test/fixtures"
     },
+//    proxy: {
+//        host: "172.22.22.22",
+//        port: 80
+//    },
     ports: {
         min: 2000
     },
