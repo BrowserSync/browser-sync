06ea73cf24eb0a2f126dea9528afa29e16aaf234
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-22 15:33:07 +0000

    Add Remove connect-livereload and create separate module

--- example.js
+++ example.js
@@ -1,17 +1,18 @@
 var browserSync = require("./lib/index");
 
-var bs = browserSync.init("test/fixtures/**/*.css", {
+var files = ["test/fixtures/**/*.css", "test/fixtures/**/*.html"];
+var bs = browserSync.init(files, {
 //    server: {
 //        baseDir: "test/fixtures"
 //    },
     proxy: {
-        host: "front.end",
+        host: "172.22.22.22",
         port: 80
     },
     ports: {
         min: 2000
     },
-    open: false,
+    open: true,
     scrollProportionally: true
 }).on("init", function (api) {
 
