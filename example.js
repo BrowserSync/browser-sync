a1eabfc7ee00759defa7bafcd38a35844dbb38e6
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-22 23:17:21 +0000

    fixes #80

--- example.js
+++ example.js
@@ -1,20 +1,7 @@
 var browserSync = require("./lib/index");
-
 var files = ["test/fixtures/**/*.css", "test/fixtures/**/*.html"];
 var bs = browserSync.init(files, {
     server: {
         baseDir: "test/fixtures"
-    },
-//    proxy: {
-//        host: "172.22.22.22"
-//    },
-    ports: {
-        min: 2000
-    },
-    open: false,
-    scrollProportionally: true
-}).on("init", function (api) {
-
-}).on("file:reload", function (file) {
-//    console.log("FILE INJECTED: " + file.assetFileName);
+    }
 });
