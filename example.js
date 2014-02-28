d6d03f3cea28afc7514a4a241e844c5a9680797b
Shane Osbourne <shane.osbourne8@gmail.com>
2014-02-28 18:59:42 +0000

    Clean up init method

--- example.js
+++ example.js
@@ -3,5 +3,9 @@ var files = ["test/fixtures/**/*.css", "test/fixtures/**/*.html"];
 var bs = browserSync.init(files, {
     server: {
         baseDir: "test/fixtures"
+    },
+    ports: {
+        min: 3000,
+        max: 3001
     }
 });
