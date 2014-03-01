bc37f11539b5787437c56e4092db8f6df783afb2
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-01 13:49:21 +0000

    Add super-test for control panel specs

--- test/server/control-panel/launch.js
+++ test/server/control-panel/launch.js
@@ -1,145 +1,41 @@
-//var bs = require("../../../lib/browser-sync");
-//var server = require("../../../lib/server");
-//var controlPanel = require("../../../lib/control-panel");
-//var proxy = require("../../../lib/proxy");
-//var path = require("path");
-//var browserSync = new bs();
-//var messages = require("../../../lib/messages");
-//var http = require("http");
-//var fs = require("fs");
-//var assert = require("chai").assert;
-//var sinon = require("sinon");
-//
-//var ports = {
-//    socket: 3000,
-//    controlPanel: 3001,
-//    server: 3002
-//};
-//var host  = "0.0.0.0";
-//
-//var cpHost   = "http://0.0.0.0:3001";
-//var clientScriptUrl = "http://0.0.0.0:" + ports.controlPanel + messages.clientScript({version:"2.3.4"});
-//var snippet = messages.scriptTags(host, ports, {}, "controlPanel");
-//
-//describe("Launching the Control panel", function () {
-//
-//    var controlPanelServer;
-//    var options, clientScriptSpy, fsStub;
-//
-//    before(function () {
-//        options = {
-//            devMode: false,
-//            version: "2.3.4"
-//        };
-//        clientScriptSpy = sinon.spy(messages, "clientScript");
-//        controlPanelServer = controlPanel.launchControlPanel("0.0.0.0", ports, options);
-//    });
-//
-//    afterEach(function () {
-//        clientScriptSpy.restore();
-//    });
-//    after(function () {
-//        controlPanelServer.close();
-//    });
-//
-//    it("should call clientScript with options", function () {
-//        sinon.assert.calledWithExactly(clientScriptSpy, options, true);
-//    });
-//
-//    it("should launch and be accessible via http", function (done) {
-//        var statusCode;
-//
-//        http.get(cpHost + "/index.html", function (res) {
-//            var actual = res.statusCode;
-//            assert.equal(actual, 200);
-//            done();
-//        });
-//    });
-//
-//    it("should launch and be able to access the control panel JS", function (done) {
-//        http.get(cpHost + "/js/app.js", function (res) {
-//            var actual = res.statusCode;
-//            assert.equal(actual, 200);
-//            done();
-//        });
-//    });
-//
-//    it("should launch and be able to access angular.min.js", function (done) {
-//        http.get(cpHost + "/js/angular.min.js", function (res) {
-//            var actual = res.statusCode;
-//            assert.equal(actual, 200);
-//            done();
-//        });
-//    });
-//
-//    it("can serve the Client JS file", function (done) {
-//        http.get(clientScriptUrl, function (res) {
-//            assert.equal(res.statusCode, 200);
-//            done();
-//        });
-//    });
-//    it("can append the shims", function (done) {
-//        http.get(clientScriptUrl, function (res) {
-//            res.on("data", function (chunk) {
-//                var respString = chunk.toString();
-//                var actual = respString.indexOf("browser-sync shims");
-//                assert.isTrue(actual > 0);
-//                done();
-//            });
-//        });
-//    });
-//    it.only("should have the snippets for socket.io + App.js attached", function (done) {
-//        var data;
-//        http.get(cpHost, function (res) {
-//            var chunks = [];
-//            res.on("data", function (chunk) {
-//                chunks.push(chunk.toString());
-//            });
-//            res.on("end", function () {
-//                data = chunks.join("");
-//                console.log(data);
-//                assert.isTrue(data.indexOf(snippet) >= 0);
-//                done();
-//            });
-//        });
-//    });
-//});
-//
-//describe("Modify Snippet", function () {
-//
-//    var readFile;
-//    var socketConnector;
-//    var clientScript;
-//
-//    before(function () {
-//        clientScript = messages.clientScript();
-//        socketConnector = sinon.spy(messages, "socketConnector");
-//        readFile = sinon.stub(fs, "readFileSync");
-//    });
-//    afterEach(function () {
-//        socketConnector.reset();
-//        readFile.reset();
-//    });
-//    after(function () {
-//        readFile.restore();
-//    });
-//    it("should be a function", function () {
-//        assert.isFunction(controlPanel.utils.modifySnippet);
-//    });
-//    it("should return a function", function () {
-//        var actual = controlPanel.utils.modifySnippet(host, ports[0]);
-//        assert.isFunction(actual);
-//    });
-//    it("should read the client JS file", function () {
-//        controlPanel.utils.modifySnippet(host, ports[0], {}, clientScript);
-//        var arg = readFile.getCall(0).args[0];
-//        var actual = arg.indexOf(messages.clientScript());
-//        assert.isTrue(actual > 0);
-//    });
-//    it("should read the client JS SHIMS file", function () {
-//        controlPanel.utils.modifySnippet(host, ports[0], {}, clientScript);
-//        var arg = readFile.getCall(1).args[0];
-//        var actual = arg.indexOf(messages.client.shims);
-//        assert.isTrue(actual > 0);
-//    });
-//});
+var bs = require("../../../lib/browser-sync");
+var server = require("../../../lib/server");
+var controlPanel = require("../../../lib/control-panel");
+var messages = require("../../../lib/messages");
+var assert = require("chai").assert;
+var request = require('supertest');
+
+var respString = "}(window, (typeof ___socket___ === \"undefined\") ? {} : ___socket___));";
+var clientJS = messages.clientScript({version:"2.3.4"});
+
+describe("Launching the Control panel", function () {
+
+    var app;
+    var options;
+
+    before(function () {
+        options = {
+            devMode: false,
+            version: "2.3.4"
+        };
+        app = controlPanel.launchControlPanel("ScriptTags", options);
+    });
+
+    it("should return the client script", function (done) {
+        request(app)
+            .get(clientJS)
+            .expect(200)
+            .expect('Content-Type', /javascript/)
+            .end(function (err, res) {
+                var actual = res.text.indexOf(respString);
+                assert.isTrue(actual >= 0);
+                done();
+            });
+    });
+
+    it("should launch and be accessible via http", function (done) {
+        request(app)
+            .get("/index.html")
+            .expect(200, done)
+    });
+});
