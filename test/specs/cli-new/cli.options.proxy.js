0476a7464353968ad34e24a1adc90ae0ec803318
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-05 08:49:15 +0000

    Add proxy option merge

--- test/specs/cli-new/cli.options.proxy.js
+++ test/specs/cli-new/cli.options.proxy.js
@@ -13,12 +13,21 @@ describe("Merging Server Options", function () {
         defaultValue = false;
     });
 
-    it("should merge default + command line options", function () {
+    it("should merge a url with HTTP", function () {
         var arg = "http://localhost:8000";
         var actual = options._mergeProxyOption(defaultValue, arg);
         var expected = {
             host: "localhost",
             port: "8000"
+        };
+        assert.deepEqual(actual, expected);
+    });
+    it("should merge a url with HTTPS", function () {
+        var arg = "https://localhost:8010";
+        var actual = options._mergeProxyOption(defaultValue, arg);
+        var expected = {
+            host: "localhost",
+            port: "8010"
         };
         assert.deepEqual(actual, expected);
     });
