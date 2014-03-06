7ff2f9f17c293e92a8d0696d6e9dcd2c3f64d2c3
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-06 21:17:54 +0000

    Add hostname option spec

---
+++ test/specs/cli-new/cli.options.files.js
@@ -0,0 +1,10 @@
+//var defaultConfig = require("../../../lib/default-config");
+//var cli = require("../../../lib/cli");
+//var init = cli.init;
+//var allowed = init.allowedOptions;
+//
+//describe("Merging Options", function () {
+//    it("should merge the files option", function () {
+//
+//    });
+//});
