7896b7f9a9347808e970a6cbc94a456d632bfbd6
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-06 21:03:49 +0000

    Add ghostmode option merge

---
+++ test/specs/cli-new/cli.options.ghost.js
@@ -0,0 +1,29 @@
+var defaultConfig = require("../../../lib/default-config");
+var cli = require("../../../lib/cli");
+var options = cli.options;
+var _ = require("lodash");
+
+var assert = require("chai").assert;
+
+describe("Merging Ghostmode option", function () {
+    it("should merge ghost mode set to false", function () {
+        var arg = false;
+        var defaultVal = defaultConfig.ghostMode;
+        var actual = options._mergeGhostModeOption(defaultVal, arg);
+        assert.equal(actual, arg);
+    });
+    it("should merge ghost mode set to false as string", function () {
+        var arg = "false";
+        var defaultVal = defaultConfig.ghostMode;
+        var actual = options._mergeGhostModeOption(defaultVal, arg);
+        assert.equal(actual, false);
+    });
+    it("should merge ghost mode option when certain options disabled", function () {
+        var arg = {
+            links: true
+        };
+        var defaultVal = defaultConfig.ghostMode;
+        var actual = options._mergeGhostModeOption(defaultVal, arg);
+        assert.equal(actual, arg);
+    });
+});
