dd776c49300580916b3cb0bcad04f685aa0fb59f
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-08 13:44:14 +0000

    Add file option merging spec

--- test/specs/cli-new/cli.options.files.js
+++ test/specs/cli-new/cli.options.files.js
@@ -1,10 +1,42 @@
-//var defaultConfig = require("../../../lib/default-config");
-//var cli = require("../../../lib/cli");
-//var init = cli.init;
-//var allowed = init.allowedOptions;
-//
-//describe("Merging Options", function () {
-//    it("should merge the files option", function () {
-//
-//    });
-//});
+var defaultConfig = require("../../../lib/default-config");
+var cli = require("../../../lib/cli");
+var options = cli.options;
+var _ = require("lodash");
+
+var assert = require("chai").assert;
+
+describe("Merging Options: Files", function () {
+    it("should return the files property from string", function () {
+        var files = "css/*.css";
+        var actual = options._mergeFilesOption(files);
+        var expected = ["css/*.css"];
+        assert.deepEqual(actual, expected);
+    });
+    it("should return the files property from array", function () {
+        var files = ["css/*.css"];
+        var actual = options._mergeFilesOption(files);
+        var expected = ["css/*.css"];
+        assert.deepEqual(actual, expected);
+    });
+    it("should return the files property from array", function () {
+        var files = ["css/*.css", "*.html"];
+        var actual = options._mergeFilesOption(files);
+        var expected = ["css/*.css", "*.html"];
+        assert.deepEqual(actual, expected);
+    });
+
+    it("should return the files property Merged with excluded array", function () {
+        var files = ["css/*.css", "*.html"];
+        var excluded = ["node_modules"];
+        var actual = options._mergeFilesOption(files, excluded);
+        var expected = ["css/*.css", "*.html", "!node_modules/**"];
+        assert.deepEqual(actual, expected);
+    });
+    it("should return the files property Merged with excluded string", function () {
+        var files = ["css/*.css", "*.html"];
+        var excluded = "node_modules";
+        var actual = options._mergeFilesOption(files, excluded);
+        var expected = ["css/*.css", "*.html", "!node_modules/**"];
+        assert.deepEqual(actual, expected);
+    });
+});
