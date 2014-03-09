f99b50f10ad0e960861fcf682cba2af01c213b02
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-09 13:44:25 +0000

    Add specs for redirect url replacement

--- test/specs/proxy/proxy.utils.js
+++ test/specs/proxy/proxy.utils.js
@@ -59,4 +59,61 @@ describe("Rewriting Domains", function () {
         var expected = "/local.dev/g";
         assert.equal(actual, expected);
     });
+
+    it("should return a full url", function () {
+        var opts = {
+            protocol: "http",
+            host: "localhost",
+            port: 80
+        };
+        var actual   = utils.getProxyUrl(opts);
+        var expected = "http://localhost";
+        assert.equal(actual, expected);
+    });
+    it("should return a full url", function () {
+        var opts = {
+            protocol: "https",
+            host: "local.dev",
+            port: 8000
+        };
+        var actual   = utils.getProxyUrl(opts);
+        var expected = "https://local.dev:8000";
+        assert.equal(actual, expected);
+    });
+
+    describe("handling redirects", function () {
+        var opts;
+        beforeEach(function () {
+            opts = {
+                protocol: "http",
+                host: "blossom.dev",
+                port: 80
+            };
+        });
+        it("should replace a 302 redirect link", function () {
+            var host = "192.168.0.5";
+            var port = 3002;
+            var url = "http://blossom.dev/index.php/install/";
+            var expected = "http://192.168.0.5:3002/index.php/install/";
+            var actual   = utils.handleRedirect(url, opts, host, port);
+            assert.equal(actual, expected);
+        });
+        it("should replace a 302 redirect link", function () {
+            var host = "192.168.0.6";
+            var port = 3003;
+            var url = "http://blossom.dev/index.php/install/";
+            var expected = "http://192.168.0.6:3003/index.php/install/";
+            var actual   = utils.handleRedirect(url, opts, host, port);
+            assert.equal(actual, expected);
+        });
+        it("should replace a 302 redirect link", function () {
+            opts.port = 8000;
+            var host = "192.168.0.6";
+            var port = 3003;
+            var url = "http://blossom.dev:8000/index.php/install/";
+            var expected = "http://192.168.0.6:3003/index.php/install/";
+            var actual   = utils.handleRedirect(url, opts, host, port);
+            assert.equal(actual, expected);
+        });
+    });
 });
