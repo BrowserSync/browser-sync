9d2565012747afae94a344fe0068a5c8992400b0
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-09 15:25:40 +0000

    Add middlewares to server option

---
+++ test/specs/server/server.middleware.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var defaultConfig = require("../../../lib/default-config");
+var server        = require("../../../lib/server");
+
+var assert  = require("chai").assert;
+var sinon   = require("sinon");
+var request = require("supertest");
+
+var ports = {
+    socket: 3000,
+    controlPanel: 3001,
+    server: 3002
+};
+
+var options = { server: {} };
+
+describe("Adding custom middleware", function () {
+
+    it("can add a single middleware given as function", function (done) {
+
+        var spy = sinon.spy(function (req, res, next) {
+            next();
+        });
+
+        options.server.baseDir = "test/fixtures";
+        options.server.middleware = spy;
+
+        var servers = server.launchServer("localhost", ports, options, {});
+
+        request(servers.staticServer)
+            .get("/index.html")
+            .expect(200)
+            .end(function () {
+                sinon.assert.called(spy);
+                done();
+            });
+    });
+    it("can add multiple middleware given as array", function (done) {
+
+        var spy = sinon.spy(function (req, res, next) {
+            next();
+        });
+        var spy2 = sinon.spy(function (req, res, next) {
+            next();
+        });
+
+        options.server.baseDir = "test/fixtures";
+        options.server.middleware = [spy, spy2];
+
+        var servers = server.launchServer("localhost", ports, options, {});
+
+        request(servers.staticServer)
+            .get("/index.html")
+            .expect(200)
+            .end(function () {
+                sinon.assert.called(spy);
+                sinon.assert.called(spy2);
+                done();
+            });
+    });
+});
