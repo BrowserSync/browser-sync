76fa1e39637db0ace40404f5ad53197e03c0f32f
Shane Osbourne <shane.osbourne8@gmail.com>
2014-03-09 15:31:08 +0000

    Add hostnameSuffix option

--- test/specs/utils/utils.xip.js
+++ test/specs/utils/utils.xip.js
@@ -4,12 +4,25 @@ var defaultConfig = require("../../../lib/default-config");
 var utils         = require("../../../lib/utils").utils;
 
 var assert = require("chai").assert;
+var sinon = require("sinon");
 
-describe("Adding xip", function () {
+describe.only("Adding xip", function () {
     it("can add xip.io when set in options", function () {
         var host = "192.15.122.9";
         var expected = "192.15.122.9.xip.io";
         var actual = utils.xip(host, {xip: true});
+        assert.equal(actual, expected);
+    });
+    it("can add xip.io using hostnameSuffix", function () {
+        var host = "192.15.122.9";
+        var expected = "192.15.122.9.xip.io";
+        var actual = utils.xip(host, {hostnameSuffix: ".xip.io"});
+        assert.equal(actual, expected);
+    });
+    it("can add random.io using hostnameSuffix", function () {
+        var host = "192.15.122.9";
+        var expected = "192.15.122.9.random.io";
+        var actual = utils.xip(host, {hostnameSuffix: ".random.io"});
         assert.equal(actual, expected);
     });
     it("does not add xip.io when using default option", function () {
