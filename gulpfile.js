27a22a7d0b2be5c151a7dfa2a5d2ffedb7f704ba
Shane Osbourne <shane.osbourne8@gmail.com>
2014-04-20 21:16:43 +0100

    added public notify method

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp        = require('gulp');
 var jshint      = require('gulp-jshint');
 var contribs    = require('gulp-contribs');
 var sass        = require('gulp-sass');
+var rubySass    = require('gulp-ruby-sass');
 var browserSync = require('./lib/index');
 
 gulp.task('lint', function () {
@@ -19,7 +20,6 @@ gulp.task('contribs', function () {
 
 gulp.task('default', ['lint']);
 
-// Example code below
 var paths = {
     scss: "test/fixtures/scss/*.scss",
     css: "test/fixtures/css",
@@ -27,35 +27,42 @@ var paths = {
 };
 
 gulp.task('sass', function () {
+    browserSync.notify("Compiling SCSS files... Please Wait");
     gulp.src(paths.scss)
-        .pipe(sass({includePaths: ['scss']}))
+        .pipe(rubySass())
         .pipe(gulp.dest(paths.css))
         .pipe(browserSync.reload({stream:true}));
 });
 
-
+/**
+ * Start BrowserSync
+ */
 gulp.task('browser-sync', function () {
 
-    var clientScript = require("/Users/shakyshane/Sites/browser-sync-modules/browser-sync-client/index");
-
-    browserSync.use("client:script", clientScript.middleware, function (err) {
-        console.log(err);
-    });
+//    var clientScript = require("/Users/shakyshane/Sites/browser-sync-modules/browser-sync-client/index");
+//
+//    browserSync.use("client:script", clientScript.middleware, function (err) {
+//        console.log(err);
+//    });
 
     browserSync.init(null, {
         server: {
             baseDir: "test/fixtures"
         },
-        startPath: "sass.html",
-        minify: false,
-        open: false
+        startPath: "sass.html"
     });
 });
 
+/**
+ * Reload task
+ */
 gulp.task('bs-reload', function () {
     browserSync.reload();
 });
 
+/**
+ * Watch stuff
+ */
 gulp.task('watch', ['browser-sync'], function () {
     gulp.watch(paths.scss, ['sass']);
     gulp.watch(paths.html, ['bs-reload']);
