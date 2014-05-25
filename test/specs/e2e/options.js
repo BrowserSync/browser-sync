7ca08a7163c711817a12591352072c722648a33e
Shane Osbourne <shane.osbourne8@gmail.com>
2014-05-25 13:09:08 +0100

    added `port` option in place of `ports` as only 1 is needed now.

--- test/specs/e2e/options.js
+++ test/specs/e2e/options.js
@@ -18,6 +18,7 @@ describe("E2E options test", function () {
             server: {
                 baseDir: __dirname + "/../../fixtures"
             },
+            port: 8080,
             debugInfo: false,
             open: false
         };
@@ -31,5 +32,42 @@ describe("E2E options test", function () {
     it("Sets the available port", function () {
         var match = /\d{2,5}/.exec(options.port)[0];
         assert.isNotNull(match);
+    });
+    it("Uses the given port the available port", function () {
+        var match = /\d{2,5}/.exec(options.port)[0];
+        assert.equal(match, 8080);
+    });
+});
+
+describe("E2E options test", function () {
+
+    var options;
+    var instance;
+
+    before(function (done) {
+        var config = {
+            server: {
+                baseDir: __dirname + "/../../fixtures"
+            },
+            ports: {
+                min: 3500
+            },
+            debugInfo: false,
+            open: false
+        };
+        browserSync.init([], config, function (err, bs) {
+            options  = bs.options;
+            instance = bs;
+            done();
+        });
+    });
+
+    it("Sets the available port", function () {
+        var match = /\d{2,5}/.exec(options.port)[0];
+        assert.isNotNull(match);
+    });
+    it("Uses the given port the available port", function () {
+        var match = /\d{2,5}/.exec(options.port)[0];
+        assert.equal(match, 3500);
     });
 });
