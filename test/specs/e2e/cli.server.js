a5cd2a15ca48ee43f1ce5458216243c6f565f205
Shane Osbourne <shane.osbourne8@gmail.com>
2014-05-25 22:07:31 +0100

    added some CLI E2E tests

---
+++ test/specs/e2e/cli.server.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var path    = require("path");
+var http    = require("http");
+var assert  = require("chai").assert;
+var spawn   = require("child_process").spawn;
+
+var index   = path.resolve( __dirname + "/../../../lib/index.js");
+
+describe("E2E server test", function () {
+
+    var bs, output;
+
+    before(function (done) {
+
+        bs = spawn("node", [index, "start", "--server", "--no-open", "--port=3001"]);
+
+        bs.stdout.on("data", function (data) {
+            output = data.toString();
+            done();
+        });
+    });
+
+    after(function () {
+        bs.kill("SIGHUP");
+    });
+
+    it("returns the snippet", function (done) {
+
+        assert.isTrue(output.indexOf("http://localhost:3001") > 0);
+
+        var options = {
+            hostname: "localhost",
+            path: "/test/fixtures/index.html",
+            port: 3001,
+            headers: {
+                accept: "text/html"
+            }
+        };
+
+        http.get(options, function (res) {
+            var chunks = [];
+            res.on("data", function (data) {
+                chunks.push(data);
+            }).on("end", function () {
+                assert.isTrue(chunks.join("").indexOf("browser-sync-client") > 0);
+                done();
+            });
+        });
+    });
+});
