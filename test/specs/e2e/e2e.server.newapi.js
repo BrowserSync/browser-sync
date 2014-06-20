8f259c47ddd63a8579c728ce06a8e012839b6fac
Shane Osbourne <shane.osbourne8@gmail.com>
2014-06-20 08:19:20 +0100

    Handle config-only init

--- test/specs/e2e/e2e.server.newapi.js
+++ test/specs/e2e/e2e.server.newapi.js
@@ -39,3 +39,50 @@ describe("E2E server test with only a callback", function () {
             });
     });
 });
+
+
+describe("E2E server test with only a config option", function () {
+
+    var options;
+    var instance;
+    var server;
+
+    before(function (done) {
+
+        var called;
+
+        instance = browserSync.init({
+            open: false,
+            debugInfo: false,
+            server: {
+                baseDir: "test/fixtures"
+            }
+        });
+
+        instance.events.on("init", function (bs) {
+
+            options = bs.options;
+            server  = bs.servers.staticServer;
+
+            if (!called) {
+                done();
+                called = true;
+            }
+        });
+    });
+
+    after(function () {
+        server.close();
+    });
+
+    it("Can return the script", function (done) {
+
+        request(server)
+            .get(options.scriptPath)
+            .expect(200)
+            .end(function (err, res) {
+                assert.isTrue(res.text.indexOf("Connected to BrowserSync") > 0);
+                done();
+            });
+    });
+});
