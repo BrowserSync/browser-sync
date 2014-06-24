c4c2c607879574f6318a7bb3f4ba3f29bb621c0d
Shane Osbourne <shane.osbourne8@gmail.com>
2014-06-24 09:08:10 +0100

    https://github.com/google/web-starter-kit/pull/171

---
+++ test/specs/api/init.active.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var browserSync = require("../../../lib/index");
+
+var assert      = require("chai").assert;
+
+describe("Retrieving the active state of browserSync", function () {
+
+    it("should know the inactive state of BrowserSync", function () {
+        assert.equal(browserSync.active, false);
+    });
+
+    describe("setting active", function () {
+
+        var instance;
+
+        before(function (done) {
+
+            var config = {
+                debugInfo: false,
+                open: false
+            };
+
+            instance = browserSync(config, done);
+        });
+
+        after(function () {
+            instance.cleanup();
+        });
+
+        it("should know the active State of BrowserSync", function () {
+            assert.equal(browserSync.active, true);
+        });
+    });
+});
