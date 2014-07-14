7b4300bcf5b28a697f5c7e5dce913ac7f5e15de6
Shane Osbourne <shane.osbourne8@gmail.com>
2014-07-14 22:51:23 +0100

    Added notify timeout option

--- test/specs/api/init.notify.js
+++ test/specs/api/init.notify.js
@@ -20,11 +20,15 @@ describe("Public Notify Method", function () {
 
     it("should emit the browser:notify event", function () {
         browserSync.notify("HI");
-        sinon.assert.calledWithExactly(emitterStub, "browser:notify", {message: "HI"});
+        sinon.assert.calledWithExactly(emitterStub, "browser:notify", {message: "HI", timeout: 2000});
     });
     it("should emit the browser:notify event (2)", function () {
         browserSync.notify("HI There");
-        sinon.assert.calledWithExactly(emitterStub, "browser:notify", {message: "HI There"});
+        sinon.assert.calledWithExactly(emitterStub, "browser:notify", {message: "HI There", timeout: 2000});
+    });
+    it("should emit the browser:notify event with a timeout", function () {
+        browserSync.notify("HI There", 3000);
+        sinon.assert.calledWithExactly(emitterStub, "browser:notify", {message: "HI There", timeout: 3000});
     });
     it("should NOT emit the browser:notify event if a message was not provided", function () {
         browserSync.notify();
