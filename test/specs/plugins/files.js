a831ea49aff69b95916e1285f32b5801661308b2
Shane Osbourne <shane.osbourne8@gmail.com>
2014-08-08 22:30:00 +0100

    Added file watching specs

---
+++ test/specs/plugins/files.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var browserSync = require("../../../index");
+
+var sinon   = require("sinon");
+var _       = require("lodash");
+var path    = require("path");
+var fs      = require("graceful-fs");
+var request = require("supertest");
+var assert  = require("chai").assert;
+
+describe("Plugins: Watching Files:", function () {
+
+
+    it("should add a namespaced watcher when given as plugin config", function (done) {
+
+        var config = {
+            debugInfo: false,
+            open: false,
+            files: "test.html"
+        };
+
+        var PLUGIN_NAME = "KITTENZ";
+
+        browserSync.use({
+            plugin: function (bs, opts) {
+                bs.events.on("file:changed", function (data) {
+                    assert.equal(data.namespace, PLUGIN_NAME);
+                    instance.cleanup();
+                    done();
+                });
+            },
+            name: PLUGIN_NAME
+        }, {files: "test.html"});
+
+        var instance = browserSync(config, function (err, bs) {
+            assert.equal(Object.keys(bs.watchers).length, 2);
+            assert.ok(bs.watchers[PLUGIN_NAME]);
+            instance.events.emit("file:changed", {namespace: PLUGIN_NAME});
+        });
+    });
+
+    it("should only add watchers if globs given", function (done) {
+
+        var config = {
+            debugInfo: false,
+            open: false
+        };
+
+        browserSync.use({
+            plugin: function (bs, opts) {}
+        }, {files: "test.html"});
+
+        var instance = browserSync(config, function (err, bs) {
+            assert.equal(Object.keys(bs.watchers).length, 1);
+            instance.cleanup();
+            done();
+        });
+    });
+
+    it("should only add watchers if globs given", function (done) {
+
+        var config = {
+            debugInfo: false,
+            open: false
+        };
+
+        browserSync.use({
+            plugin: function (bs, opts) {}
+        });
+
+        var instance = browserSync(config, function (err, bs) {
+            assert.equal(Object.keys(bs.watchers).length, 0);
+            instance.cleanup();
+            done();
+        });
+    });
+});
