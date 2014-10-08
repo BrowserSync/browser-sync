a82be8cf3343941ee173c1ae1aa708905b206dc6
Shane Osbourne <shane@wearejh.com>
2014-10-08 13:31:54 +0100

    Update eazy-logger to allow `logPrefix` to accept a function. https://github.com/shakyShane/easy-logger/issues/1

--- test/specs/e2e/e2e.options.logPrefix.js
+++ test/specs/e2e/e2e.options.logPrefix.js
@@ -34,3 +34,32 @@ describe("E2E `logPrefix` option", function () {
         assert.notInclude(stripColor(arg), "[BS]");
     });
 });
+describe("E2E `logPrefix` option (function)", function () {
+
+    var instance;
+    var spy;
+
+    before(function (done) {
+        var config = {
+            open: false,
+            logPrefix: function () {
+                return this.compile("AWESOME");
+            },
+            logLevel:  "info"
+        };
+        logger.mute(false);
+        spy      = sinon.spy(console, "log");
+        instance = browserSync(config, done);
+    });
+
+    after(function () {
+        instance.cleanup();
+        console.log.restore();
+    });
+
+    it("Can set the log prefix with a function when given in options", function () {
+        var arg = spy.getCall(0).args[0];
+        assert.include(stripColor(arg), "AWESOME");
+        assert.notInclude(stripColor(arg), "[BS]");
+    });
+});
