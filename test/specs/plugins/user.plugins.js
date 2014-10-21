244276fdc616a8c00182f44b91ada837ab72da1b
Shane Osbourne <shane.osbourne8@gmail.com>
2014-10-21 17:07:20 +0100

    Add spec for enable/disable plugins from event

--- test/specs/plugins/user.plugins.js
+++ test/specs/plugins/user.plugins.js
@@ -35,4 +35,21 @@ describe("Plugins: Retrieving user plugins", function () {
         assert.equal(plugin.name, PLUGIN_NAME);
         done();
     });
+    it("Disable/re-enable a plugin from event", function (done) {
+        assert.isTrue(instance.pluginManager.getPlugin(PLUGIN_NAME)._enabled);
+        instance.events.emit("plugins:configure", {
+            name: PLUGIN_NAME,
+            active: false
+        });
+        assert.isFalse(instance.options.userPlugins[0].active);
+        assert.isFalse(instance.pluginManager.getPlugin(PLUGIN_NAME)._enabled);
+
+        instance.events.emit("plugins:configure", {
+            name: PLUGIN_NAME,
+            active: true
+        });
+        assert.isTrue(instance.options.userPlugins[0].active);
+        assert.isTrue(instance.pluginManager.getPlugin(PLUGIN_NAME)._enabled);
+        done();
+    });
 });
