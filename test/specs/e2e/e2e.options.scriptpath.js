9e02b8df7ea723dd2e91136cc17f611aef43078f
Shane Osbourne <shane@wearejh.com>
2014-10-29 16:40:29 +0000

    Make both scriptPath and socket.namespace configurable - fix #274

--- test/specs/e2e/e2e.options.scriptpath.js
+++ test/specs/e2e/e2e.options.scriptpath.js
@@ -1,9 +1,9 @@
 "use strict";
 
 var browserSync = require("../../../index");
-var messages = require("../../../lib/messages");
 
 var assert      = require("chai").assert;
+var request     = require("supertest");
 
 describe("E2E script path test - given a callback", function () {
 
@@ -16,8 +16,8 @@ describe("E2E script path test - given a callback", function () {
                 baseDir: "test/fixtures"
             },
             open: false,
-            scriptPath: function (path) {
-                return path;
+            scriptPath: function (scriptPath) {
+                return "localhost:PORT" + scriptPath;
             }
         };
         instance = browserSync(config, done);
@@ -27,8 +27,76 @@ describe("E2E script path test - given a callback", function () {
         instance.cleanup();
     });
 
-    it("Sets the available port", function () {
-        var script = messages.clientScript({version: instance.version});
-        assert.include(instance.options.snippet, "src='" + script + "'");
+    it("Sets the script path", function () {
+        assert.include(instance.options.snippet, "localhost:PORT/browser-sync/browser-sync-client.");
+    });
+});
+
+describe("E2E Socket path test - given a callback", function () {
+
+    var instance;
+
+    before(function (done) {
+
+        var config = {
+            server: {
+                baseDir: "test/fixtures"
+            },
+            open: false,
+            socket: {
+                namespace: function (namespace) {
+                    return namespace + "TEST";
+                }
+            }
+        };
+        instance = browserSync(config, done);
+    });
+
+    after(function () {
+        instance.cleanup();
+    });
+
+    it("sets the socket path", function (done) {
+        request(instance.server)
+            .get(instance.options.scriptPaths.path)
+            .expect(200)
+            .end(function (err, res) {
+                assert.include(res.text, "io('/browser-sync/socket.ioTEST'");
+                done();
+            });
+    });
+});
+describe("E2E Socket path test - given a callback 2", function () {
+
+    var instance;
+
+    before(function (done) {
+
+        var config = {
+            server: {
+                baseDir: "test/fixtures"
+            },
+            open: false,
+            socket: {
+                namespace: function (namespace) {
+                    return "localhost:3003" + namespace;
+                }
+            }
+        };
+        instance = browserSync(config, done);
+    });
+
+    after(function () {
+        instance.cleanup();
+    });
+
+    it("sets the socket path", function (done) {
+        request(instance.server)
+            .get(instance.options.scriptPaths.path)
+            .expect(200)
+            .end(function (err, res) {
+                assert.include(res.text, "___browserSync___.io('localhost:3003/browser-sync/socket.io',");
+                done();
+            });
     });
 });
