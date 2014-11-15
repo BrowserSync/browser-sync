890e5c7ecb5fc1127a535d2dc2b886024e53b962
Shane Osbourne <shane.osbourne8@gmail.com>
2014-11-15 16:45:51 +0000

    Add input + click tests

--- test/protractor/tests/scroll.js
+++ test/protractor/tests/scroll.js
@@ -1,5 +1,6 @@
 var init = require("../bs.init");
 var path = require("path");
+var assert = require("chai").assert;
 
 describe('Page Navigation', function() {
     var ptor     = protractor.getInstance();
@@ -28,9 +29,46 @@ describe('Page Navigation', function() {
             assertScripts();
         });
     });
-    it("should navigate to other pages", function () {
+    it("should know when a client scrolls", function () {
+
+        var flow = protractor.promise.controlFlow();
+        var deferred = protractor.promise.defer();
+
+        instance.io.sockets.on("connection", function (client) {
+            client.on("scroll", function (data) {
+                expect(data.position.raw.y).toBe(100);
+            });
+            client.on("click", function (data) {
+                expect(data.tagName).toEqual("A");
+                expect(data.index).toEqual(0);
+                expect(data.url).toEqual("/scrolling.html");
+            });
+        });
+
         browser.get(urls.local + "/scrolling.html");
-        assertScripts();
+        ptor.executeScript("window.scrollBy(0, 100);");
+        var elem = element(by.css('a'));
+        elem.click();
+    });
+
+    it("should know when a client is filling a form", function () {
+
+        instance.io.sockets.on("connection", function (client) {
+            var keyCount = 0;
+            client.on("input:text", function (data) {
+                if (data.value === "Hi there") {
+                    expect(data.tagName).toEqual("INPUT");
+                    expect(data.index).toEqual(0);
+                    expect(data.url).toEqual("/forms.html");
+                    expect(keyCount).toEqual(8);
+                }
+                keyCount += 1;
+            });
+        });
+
+        browser.get(urls.local + "/forms.html");
+        element(by.id("name")).sendKeys("Hi there");
+        //ptor.pause();
     });
 });
 
