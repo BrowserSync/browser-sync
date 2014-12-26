13fc65d47a8440d2c772b68a2c2411e09bf5f008
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-26 20:59:39 +0000

    option-merging specs

--- test/specs/cli/cli.options.ghost.js
+++ test/specs/cli/cli.options.ghost.js
@@ -3,33 +3,81 @@
 var defaultConfig   = require("../../../lib/default-config");
 var cli             = require("../../../lib/cli/");
 var options         = cli.options;
+var merge           = cli.options.merge;
+var Immutable       = require("immutable");
 var callbacks       = options.callbacks;
 
 var assert = require("chai").assert;
 
 describe("CLI: Options: Merging Ghostmode options", function () {
     it("should merge ghost mode set to false from cli", function () {
-        var arg = false;
-        var defaultVal = defaultConfig.ghostMode;
-        var actual = callbacks.ghostMode(defaultVal, arg, arg, {ghost: false});
-        assert.equal(actual.forms.submit, false);
-        assert.equal(actual.forms.inputs, false);
-        assert.equal(actual.forms.toggles, false);
+        var imm = merge({
+            ghostMode: {
+                clicks: false
+            }
+        });
+        assert.isFalse(imm.getIn(["ghostMode", "clicks"]));
+        assert.isTrue(imm.getIn(["ghostMode", "scroll"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "submit"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "inputs"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "toggles"]));
+    });
+    it("should merge nested ghost mode prop", function () {
+        var imm = merge({
+            ghostMode: {
+                forms: {
+                    submit: false
+                }
+            }
+        });
+        assert.isTrue(imm.getIn(["ghostMode", "clicks"]));
+        assert.isTrue(imm.getIn(["ghostMode", "scroll"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "submit"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "inputs"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "toggles"]));
     });
     it("should merge ghost mode set to false", function () {
-        var arg = false;
-        var defaultVal = defaultConfig.ghostMode;
-        var actual = callbacks.ghostMode(defaultVal, arg, arg);
-        assert.equal(actual.forms.submit, false);
-        assert.equal(actual.forms.inputs, false);
-        assert.equal(actual.forms.toggles, false);
+        var imm = merge({
+            ghostMode: false
+        });
+        assert.isFalse(imm.getIn(["ghostMode", "clicks"]));
+        assert.isFalse(imm.getIn(["ghostMode", "scroll"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "submit"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "inputs"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "toggles"]));
     });
-    it("should merge ghost mode set to false as string", function () {
-        var arg = "false";
-        var defaultVal = defaultConfig.ghostMode;
-        var actual = callbacks.ghostMode(defaultVal, arg, arg);
-        assert.equal(actual.forms.submit, false);
-        assert.equal(actual.forms.inputs, false);
-        assert.equal(actual.forms.toggles, false);
+    it("should merge ghost mode set to true", function () {
+        var imm = merge({
+            ghostMode: true
+        });
+        assert.isTrue(imm.getIn(["ghostMode", "clicks"]));
+        assert.isTrue(imm.getIn(["ghostMode", "scroll"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "submit"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "inputs"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "toggles"]));
+    });
+    it("should merge ghost mode forms set to false", function () {
+        var imm = merge({
+            ghostMode: {
+                forms: false
+            }
+        });
+        assert.isTrue(imm.getIn(["ghostMode", "clicks"]));
+        assert.isTrue(imm.getIn(["ghostMode", "scroll"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "submit"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "inputs"]));
+        assert.isFalse(imm.getIn(["ghostMode", "forms", "toggles"]));
+    });
+    it("should merge ghost mode forms set to true", function () {
+        var imm = merge({
+            ghostMode: {
+                forms: true
+            }
+        });
+        assert.isTrue(imm.getIn(["ghostMode", "clicks"]));
+        assert.isTrue(imm.getIn(["ghostMode", "scroll"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "submit"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "inputs"]));
+        assert.isTrue(imm.getIn(["ghostMode", "forms", "toggles"]));
     });
 });
