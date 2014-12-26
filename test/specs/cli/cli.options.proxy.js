78a1d5be647c6b40a8de39ddda7b2d636c1018c4
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-26 18:15:04 +0000

    Proxy merging specs

--- test/specs/cli/cli.options.proxy.js
+++ test/specs/cli/cli.options.proxy.js
@@ -14,7 +14,9 @@ describe("CLI: Options: Merging Proxy Options", function () {
     });
 
     it("should merge a url with HTTP", function () {
-        var imm = merge({proxy: "http://localhost:8000"});
+        var imm = merge({
+            proxy: "http://localhost:8000"
+        });
         assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "http",
             host: "localhost",
@@ -23,96 +25,95 @@ describe("CLI: Options: Merging Proxy Options", function () {
         });
     });
     it("should merge a url with HTTPS", function () {
-        var arg = "https://localhost:8010";
-        var actual = options.callbacks.proxy(defaultValue, arg);
-        var expected = {
+        var imm = merge({
+            proxy: "https://localhost:8010"
+        });
+        assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "https",
             host: "localhost",
             port: 8010,
             target: "https://localhost:8010"
-        };
-        assert.deepEqual(actual, expected);
+        });
     });
     it("should merge a url with no protocol", function () {
-        var arg = "localhost:8010";
-        var actual = options.callbacks.proxy(defaultValue, arg);
-        var expected = {
+        var imm = merge({
+            proxy: "localhost:8010"
+        });
+        assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "http",
             host: "localhost",
             port: 8010,
             target: "http://localhost:8010"
-        };
-        assert.deepEqual(actual, expected);
+        });
     });
     it("should merge IP hostname", function () {
-        var arg = "192.168.0.4";
-        var actual = options.callbacks.proxy(defaultValue, arg);
-        var expected = {
+        var imm = merge({
+            proxy: "192.168.0.4"
+        });
+        assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "http",
             host: "192.168.0.4",
             port: 80,
             target: "http://192.168.0.4"
-        };
-        assert.deepEqual(actual, expected);
+        });
     });
     it("should merge IP hostname with port", function () {
-        var arg = "192.168.0.4:9001";
-        var actual = options.callbacks.proxy(defaultValue, arg);
-        var expected = {
+        var imm = merge({
+            proxy: "192.168.0.4:9001"
+        });
+        assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "http",
             host: "192.168.0.4",
             port: 9001,
             target: "http://192.168.0.4:9001"
-        };
-        assert.deepEqual(actual, expected);
+        });
     });
     it("should merge a url with no protocol & no port", function () {
-        var arg = "localhost";
-        var actual = options.callbacks.proxy(defaultValue, arg);
-        var expected = {
+        var imm = merge({
+            proxy: "localhost"
+        });
+        assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "http",
             host: "localhost",
             port: 80,
             target: "http://localhost"
-        };
-        assert.deepEqual(actual, expected);
+        });
     });
     it("should merge a url with no protocol & no port", function () {
-        var arg = "local.dev";
-        var actual = options.callbacks.proxy(defaultValue, arg);
-        var expected = {
+        var imm = merge({
+            proxy: "local.dev"
+        });
+        assert.deepEqual(imm.get("proxy").toJS(), {
             protocol: "http",
             host: "local.dev",
             port: 80,
             target: "http://local.dev"
-        };
-        assert.deepEqual(actual, expected);
+        });
     });
-
     describe("Setting the start path", function () {
         it("should merge a url with path", function () {
-            var arg = "http://local.dev/subdir";
-            var actual = options.callbacks.proxy(defaultValue, arg);
-            var expected = {
+            var imm = merge({
+                proxy: "http://local.dev/subdir"
+            });
+            assert.deepEqual(imm.get("proxy").toJS(), {
                 protocol: "http",
                 host: "local.dev",
                 port: 80,
                 startPath: "subdir",
                 target: "http://local.dev"
-            };
-            assert.deepEqual(actual, expected);
+            });
         });
         it("should merge a url with path & query params", function () {
-            var arg = "http://local.dev/subdir/another/path?rel=123";
-            var actual = options.callbacks.proxy(defaultValue, arg);
-            var expected = {
+            var imm = merge({
+                proxy: "http://local.dev/subdir/another/path?rel=123"
+            });
+            assert.deepEqual(imm.get("proxy").toJS(), {
                 protocol: "http",
                 host: "local.dev",
                 port: 80,
                 startPath: "subdir/another/path?rel=123",
                 target: "http://local.dev"
-            };
-            assert.deepEqual(actual, expected);
+            });
         });
     });
 });
