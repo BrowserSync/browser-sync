34131135dbb0f97168709037707ef1cbb02129d0
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-26 20:12:09 +0000

    Files option specs

--- test/specs/cli/cli.options.files.js
+++ test/specs/cli/cli.options.files.js
@@ -3,59 +3,71 @@
 var cli             = require("../../../lib/cli/");
 var options         = cli.options;
 var callbacks       = options.callbacks;
+var merge           = cli.options.merge;
 
 var assert = require("chai").assert;
 
 describe("CLI: Options: Merging Options: Files", function () {
     it("should return the files property from string", function () {
-        var files = "css/*.css";
-        var actual = callbacks.files([], files);
-        var expected = ["css/*.css"];
-        assert.deepEqual(actual, expected);
+        var imm = merge({files:["css/*.css"]});
+        assert.deepEqual(imm.get("files").toJS(), ["css/*.css"]);
     });
     it("should return the files property from string", function () {
-        var files = "css/*.css, *.html";
-        var actual = callbacks.files([], files);
-        var expected = ["css/*.css", "*.html"];
-        assert.deepEqual(actual, expected);
+        var imm = merge({}, {
+            files: "css/*.css, *.html"
+        });
+        assert.deepEqual(
+            imm.get("files").toJS(),
+            ["css/*.css", "*.html"]
+        );
     });
     it("should return the files property from string", function () {
-        var files = "css/*.css, test/fixtures/*.html";
-        var actual = callbacks.files([], files);
-        var expected = ["css/*.css", "test/fixtures/*.html"];
-        assert.deepEqual(actual, expected);
+        var imm = merge({}, {
+            files: "css/*.css, test/fixtures/*.html"
+        });
+        assert.deepEqual(
+            imm.get("files").toJS(),
+            ["css/*.css", "test/fixtures/*.html"]
+        );
     });
     it("should return the files property from array", function () {
-        var files = ["css/*.css"];
-        var actual = callbacks.files([], files);
-        var expected = ["css/*.css"];
-        assert.deepEqual(actual, expected);
+        var imm = merge({files: ["css/*.css"]});
+        assert.deepEqual(
+            imm.get("files").toJS(),
+            ["css/*.css"]
+        );
     });
     it("should return the files property from array", function () {
-        var files = ["css/*.css", "*.html"];
-        var actual = callbacks.files([], files);
-        var expected = ["css/*.css", "*.html"];
-        assert.deepEqual(actual, expected);
+        var imm = merge({files: ["css/*.css", "*.html"]});
+        assert.deepEqual(
+            imm.get("files").toJS(),
+            ["css/*.css", "*.html"]
+        );
     });
-
-    it("should return the files property Merged with excluded array", function () {
-        var files = ["css/*.css", "*.html"];
-        var excluded = ["node_modules"];
-        var actual = callbacks.files([], files, null, {exclude: excluded});
-        var expected = ["css/*.css", "*.html", "!node_modules/**"];
-        assert.deepEqual(actual, expected);
+    it("should return the files property Merged with excluded array (cli)", function () {
+        var imm = merge({}, {
+            files: "css/*.css, *.html",
+            exclude: "node_modules"
+        });
+        assert.deepEqual(
+            imm.get("files").toJS(),
+            ["css/*.css", "*.html", "!node_modules/**"]
+        );
     });
-    it("should return the files property Merged with excluded string", function () {
-        var files = ["css/*.css", "*.html"];
-        var excluded = "node_modules";
-        var actual = callbacks.files([], files, null, {exclude: excluded});
-        var expected = ["css/*.css", "*.html", "!node_modules/**"];
-        assert.deepEqual(actual, expected);
+    it("should return the files property Merged with excluded array (cli)", function () {
+        var imm = merge({}, {
+            files: "css/*.css, *.html",
+            exclude: ["node_modules"]
+        });
+        assert.deepEqual(
+            imm.get("files").toJS(),
+            ["css/*.css", "*.html", "!node_modules/**"]
+        );
     });
     it("should not break if 'exclude' option is provided without 'files' option", function () {
-        var excluded = "node_modules";
-        var actual = callbacks.files([], null, null, {exclude: excluded});
-        var expected = [];
-        assert.deepEqual(actual, expected);
+        var imm = merge({}, {
+            exclude: "node_modules"
+        });
+        assert.isFalse(imm.get("files"));
     });
 });
