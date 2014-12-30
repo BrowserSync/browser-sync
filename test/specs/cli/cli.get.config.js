25155c31e998145f26a36a77c2fa9bb8b878bb07
Shane Osbourne <shane.osbourne8@gmail.com>
2014-12-30 18:35:23 +0000

    Fix CLI specs

--- test/specs/cli/cli.get.config.js
+++ test/specs/cli/cli.get.config.js
@@ -1,30 +1,17 @@
 "use strict";
 
-var loadedConfig = require("../../../lib/default-config");
-var info         = require("../../../lib/cli/cli-info");
+var info           = require("../../../lib/cli/cli-info");
+var assert         = require("chai").assert;
 
-var _            = require("lodash");
-var assert       = require("chai").assert;
+describe("When reading a config file from the file system", function () {
 
-var configFilePath = "test/fixtures/config/si-config.js";
-
-describe("CLI: Resolving Config file:", function () {
-
-    var defaultConfig;
-
-    beforeEach(function () {
-        defaultConfig = _.cloneDeep(loadedConfig);
+    it("should throw if the file is not found", function () {
+        assert.throws(function () {
+            info.getConfigFile("random/file/doesn'texist");
+        });
     });
 
-    describe("When reading a config file from the file system", function () {
-
-        it("should return false if the file is not found", function () {
-            var files = info._getConfigFile("random/file/doesn'texist");
-            assert.isFalse(files);
-        });
-
-        it("should not throw if the file is found", function () {
-            assert.isDefined(info._getConfigFile(configFilePath));
-        });
+    it("should not throw if the file is found", function () {
+        assert.isDefined(info.getConfigFile("test/fixtures/config/si-config.js"));
     });
 });
