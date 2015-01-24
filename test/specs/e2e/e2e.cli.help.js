24a40a0603f67fdd982b3f14da46ba20c1a30a94
Shane Osbourne <shane.osbourne8@gmail.com>
2015-01-24 16:21:37 +0000

    Added e2e cli tests for  & help

--- test/specs/e2e/e2e.cli.help.js
+++ test/specs/e2e/e2e.cli.help.js
@@ -3,13 +3,13 @@
 var assert  = require("chai").assert;
 var exec    = require("child_process").execFile;
 
-describe("E2E CLI init test", function () {
+describe("E2E CLI help test", function () {
 
     var bs;
     var chunks = [];
 
     before(function (done) {
-        bs = exec(__dirname + "/../../../bin/browser-sync.js", ["init"], function () {
+        bs = exec(__dirname + "/../../../bin/browser-sync.js", function () {
             bs.emit("end");
             done();
         });
@@ -18,7 +18,8 @@ describe("E2E CLI init test", function () {
         });
     });
 
-    it("creates a config file in cwd", function () {
-        assert.include(chunks.join(""), "Config file created bs-config.js");
+    it("returns the help text when no commands given", function (done) {
+        assert.include(chunks.join(""), "Server Example:");
+        done();
     });
 });
--- test/specs/e2e/e2e.cli.server.js
+++ test/specs/e2e/e2e.cli.server.js
@@ -1,65 +1,54 @@
-"use strict";
-
-var path     = require("path");
-var request  = require("supertest");
-var server   = require("./commands.server.json");
-var assert   = require("chai").assert;
-var exec     = require("child_process").exec;
-
-var index   = path.resolve(__dirname + "/../../../index.js");
-
-describe.skip("E2E CLI server test", function () {
-
-    var bs, options;
-
-    before(function (done) {
-
-        bs = exec("node " + index, server.commands[0].args);
-
-        bs.stdout.on("data", function (data) {
-            console.log(data);
-            done();
-        });
-//        bs.on("message", function (data) {
-//            options = data.options;
+//"use strict";
+//
+//var request = require("supertest");
+//var server  = require("./commands.server.json");
+//var assert  = require("chai").assert;
+//var exec    = require("child_process").execFile;
+//
+//describe.only("E2E CLI server test", function () {
+//
+//    var bs, options;
+//
+//    before(function (done) {
+//        bs = exec(__dirname + "/../../../bin/browser-sync.js", ["init"], function (err, out) {
+//            //console.log(err.message);
+//            console.log(out);
 //            done();
 //        });
-//        bs.send({send: "options"});
-    });
-
-    after(function () {
-        bs.close();
-    });
-
-    it("returns the snippet", function (done) {
-
-        request(options.urls.local)
-            .get(options.scriptPaths.versioned)
-            .expect(200)
-            .end(function (err, res) {
-                assert.include(res.text, "Connected to BrowserSync");
-                done();
-            });
-    });
-
-    it("Can serve files", function (done) {
-        request(options.urls.local)
-            .get("/")
-            .expect(200)
-            .end(function (err, res) {
-                assert.include(res.text, "BrowserSync + Public URL");
-                done();
-            });
-    });
-
-    it("Can serve files with snippet added", function (done) {
-        request(options.urls.local)
-            .get("/")
-            .set("accept", "text/html")
-            .expect(200)
-            .end(function (err, res) {
-                assert.include(res.text, options.snippet);
-                done();
-            });
-    });
-});
+//        bs.emit("end");
+//    });
+//
+//    it("returns the snippet", function (done) {
+//
+//        //console.log("HERE");
+//        done();
+//        //request(options.urls.local)
+//        //    .get(options.scriptPaths.versioned)
+//        //    .expect(200)
+//        //    .end(function (err, res) {
+//        //        assert.include(res.text, "Connected to BrowserSync");
+//        //        done();
+//        //    });
+//    });
+//
+//    //it("Can serve files", function (done) {
+//    //    request(options.urls.local)
+//    //        .get("/")
+//    //        .expect(200)
+//    //        .end(function (err, res) {
+//    //            assert.include(res.text, "BrowserSync + Public URL");
+//    //            done();
+//    //        });
+//    //});
+//    //
+//    //it("Can serve files with snippet added", function (done) {
+//    //    request(options.urls.local)
+//    //        .get("/")
+//    //        .set("accept", "text/html")
+//    //        .expect(200)
+//    //        .end(function (err, res) {
+//    //            assert.include(res.text, options.snippet);
+//    //            done();
+//    //        });
+//    //});
+//});
