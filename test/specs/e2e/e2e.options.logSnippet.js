53dd3cb2e4c3f91faf26769504144f2318282342
Shane Osbourne <shane.osbourne8@gmail.com>
2015-01-29 19:35:14 +0000

    Make logger pretty

--- test/specs/e2e/e2e.options.logSnippet.js
+++ test/specs/e2e/e2e.options.logSnippet.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var browserSync = require("../../../index");
-var assert      = require("chai").assert;
 var sinon       = require("sinon");
 
 describe("E2E `logSnippet` option", function () {
@@ -10,12 +9,15 @@ describe("E2E `logSnippet` option", function () {
     var spy;
 
     before(function (done) {
+
         browserSync.reset();
+
         var config = {
             online: false,
             open: false,
             logSnippet: false
         };
+
         spy      = sinon.spy(console, "log");
         instance = browserSync(config, done).instance;
     });
@@ -26,9 +28,6 @@ describe("E2E `logSnippet` option", function () {
     });
 
     it("Can set the log snippet when given in options", function () {
-        var args = spy.getCall(0).args;
-        args.forEach(function (arg) {
-            assert.notInclude(String(arg), "browser-sync-client");
-        });
+        sinon.assert.notCalled(spy);
     });
 });
