1a543129402c3d451bb8f0c6bfeaeba5e5f702f4
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-03 11:43:05 +0000

    Add E2E snippet serving spec

---
+++ test/specs/e2e/cli/e2e.cli.snippet.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var path        = require("path");
+var request     = require("supertest");
+var assert      = require("chai").assert;
+var browserSync = require(path.resolve("./"));
+
+var pkg         = require(path.resolve("package.json"));
+var cli         = require(path.resolve(pkg.bin));
+
+describe("E2E CLI Snippet test", function () {
+
+    var instance;
+
+    before(function (done) {
+
+        browserSync.reset();
+
+        cli({
+            cli: {
+                input: ["start"],
+                flags: {
+                    logLevel: "silent"
+                }
+            },
+            cb: function (err, bs) {
+                instance = bs;
+                done();
+            }
+        });
+    });
+    after(function () {
+        instance.cleanup();
+    });
+    it("serves versioned browser-sync client js", function (done) {
+        request(instance.server)
+            .get(instance.options.getIn(["scriptPaths", "versioned"]))
+            .set("accept", "text/html")
+            .expect(200)
+            .end(function (err, res) {
+                assert.include(res.text, "Connected to BrowserSync");
+                done();
+            });
+    });
+    it("serves browser-sync client js", function (done) {
+        request(instance.server)
+            .get(instance.options.getIn(["scriptPaths", "path"]))
+            .set("accept", "text/html")
+            .expect(200)
+            .end(function (err, res) {
+                assert.include(res.text, "Connected to BrowserSync");
+                done();
+            });
+    });
+});
