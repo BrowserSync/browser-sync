18324f0a7b4d3c49bd16800a7ba77cf13ea2449a
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-05 14:08:51 +0000

    fix(cli): explode files args when given on command line. fixes #425, fixes #426

--- test/specs/e2e/cli/e2e.cli.files.js
+++ test/specs/e2e/cli/e2e.cli.files.js
@@ -1,13 +1,14 @@
 "use strict";
 
 var path        = require("path");
+var utils       = require("../../../../lib/utils");
 var assert      = require("chai").assert;
 var browserSync = require(path.resolve("./"));
 
 var pkg         = require(path.resolve("package.json"));
 var cli         = require(path.resolve(pkg.bin));
 
-describe("E2E CLI `files` arg", function () {
+describe("E2E CLI `files` arg - multi globs", function () {
 
     var instance;
 
@@ -33,7 +34,44 @@ describe("E2E CLI `files` arg", function () {
     after(function () {
         instance.cleanup();
     });
-    it.only("Converts cli files arg to correct namespaced watchers", function () {
+    it("Converts cli files arg to correct namespaced watchers", function () {
         assert.equal(instance.options.getIn(["files", "core"]).size, 2);
+        assert.equal(instance.watchers.core.glob.size, 2);
+
+        assert.isTrue(utils.isList(instance.watchers.core.glob));
+    });
+});
+
+describe("E2E CLI `files` arg, single glob", function () {
+
+    var instance;
+
+    before(function (done) {
+
+        browserSync.reset();
+
+        cli({
+            cli: {
+                input: ["start"],
+                flags: {
+                    logLevel: "silent",
+                    open: false,
+                    files: "*.html"
+                }
+            },
+            cb: function (err, bs) {
+                instance = bs;
+                done();
+            }
+        });
+    });
+    after(function () {
+        instance.cleanup();
+    });
+    it("Converts cli files arg to correct namespaced watchers", function () {
+        assert.equal(instance.options.getIn(["files", "core"]).size, 1);
+        assert.equal(instance.watchers.core.glob.size, 1);
+
+        assert.isTrue(utils.isList(instance.watchers.core.glob));
     });
 });
