c9c192d2d7b6886448607d573f2fc05e254f6bf1
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-07 10:44:09 +0000

    Revert "feat(tunnel): Switch to ngrok - re: #192"
    This reverts commit 7359435ca4efd429c9421aa912a036f82d022d82.

--- test/specs/e2e/server/e2e.server.tunnel.js
+++ test/specs/e2e/server/e2e.server.tunnel.js
@@ -3,113 +3,33 @@
 var browserSync = require("../../../../index");
 
 var assert = require("chai").assert;
-var sinon  = require("sinon");
-var ngrok  = require("ngrok");
 
-describe("Tunnel e2e tests", function () {
+describe.skip("Tunnel e2e tests", function () {
 
-    var bs;
+    describe("E2E server test with tunnel", function () {
 
-    before(function (done) {
+        var instance;
 
-        browserSync.reset();
+        before(function (done) {
+            browserSync.reset();
+            var config = {
+                server:    {
+                    baseDir: "test/fixtures"
+                },
+                logLevel: "silent",
+                open:      false,
+                tunnel:    true,
+                online:    true
+            };
+            instance = browserSync(config, done).instance;
+        });
 
-        var config = {
-            server:    {
-                baseDir: "test/fixtures"
-            },
-            logLevel: "silent",
-            open:      false,
-            tunnel:    true,
-            online:    true
-        };
-        bs = browserSync(config, done).instance;
-    });
+        after(function () {
+            instance.cleanup();
+        });
 
-    after(function () {
-        bs.cleanup();
-        bs.tunnel.disconnect();
-    });
-
-    it("should call init on the tunnel", function () {
-        assert.include(bs.options.getIn(["urls", "tunnel"]), "ngrok");
-    });
-});
-
-describe("Tunnel e2e tests passing config", function () {
-
-    var bs, stub;
-
-    before(function (done) {
-
-        browserSync.reset();
-
-        stub = sinon
-            .stub(ngrok, "connect")
-            .yields(null, "http://shane.com");
-
-        var config = {
-            server:    {
-                baseDir: "test/fixtures"
-            },
-            logLevel: "silent",
-            open:      false,
-            online:    true,
-            tunnel:    {
-                authtoken: "shakyshane",
-                somevar:   9000
-            }
-        };
-        bs = browserSync(config, done).instance;
-    });
-
-    after(function () {
-        bs.cleanup();
-        ngrok.connect.restore();
-    });
-
-    it("should call init on the tunnel", function () {
-        assert.equal(bs.options.getIn(["tunnel", "authtoken"]), "shakyshane");
-        assert.equal(bs.options.getIn(["tunnel", "somevar"]), 9000);
-        assert.equal(bs.options.getIn(["urls",   "tunnel"]), "http://shane.com");
-    });
-});
-
-describe("Tunnel e2e tests - handling errors", function () {
-
-    var bs, stub;
-
-    before(function (done) {
-
-        browserSync.reset();
-
-        stub = sinon
-            .stub(ngrok, "connect")
-            .yields(new Error("Cannot connect"));
-
-        var config = {
-            server:    {
-                baseDir: "test/fixtures"
-            },
-            logLevel: "silent",
-            open:      false,
-            online:    true,
-            tunnel:    {
-                authtoken: "shakyshane",
-                somevar:   9000
-            }
-        };
-
-        bs = browserSync(config, done).instance;
-    });
-
-    after(function () {
-        bs.cleanup();
-        ngrok.connect.restore();
-    });
-
-    it("should call init on the tunnel", function () {
-        assert.isUndefined(bs.getOptionIn(["urls", "tunnel"]));
-        assert.isUndefined(bs.tunnel);
+        it("should call init on the tunnel", function () {
+            assert.include(instance.options.getIn(["urls", "tunnel"]), "localtunnel.me");
+        });
     });
 });
