91e48a48e3ca1a225447195c9a6621936ac9707f
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-26 22:20:22 +0000

    tests(options): Add test for socket.namespace as fn

---
+++ test/specs/e2e/e2e.options.sockets.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var browserSync = require("../../../index");
+
+var assert = require("chai").assert;
+var request = require("supertest");
+
+describe("E2E `socket` options", function () {
+
+    var instance;
+
+    before(function (done) {
+        browserSync.reset();
+        var config = {
+            logLevel: "silent",
+            server:    "test/fixtures",
+            online:    false,
+            open:      false,
+            socket: {
+                namespace: function () {
+                    return "/shane";
+                }
+            }
+        };
+        instance = browserSync(config, done).instance;
+    });
+
+    after(function () {
+        instance.cleanup();
+    });
+
+    it("calls namespace fn when given in socket options", function (done) {
+        request(instance.server)
+            .get(instance.options.getIn(["scriptPaths", "versioned"]))
+            .expect(200)
+            .end(function (err, res) {
+                assert.include(res.text, "location.host + '/shane'");
+                done();
+            });
+    });
+});
