d60a1fd597027497b005a194d0fc71b7390f60c3
Shane Osbourne <shane.osbourne8@gmail.com>
2015-02-26 22:27:19 +0000

    tests(tunnel): Add test for tunnel + subdomain

--- test/specs/e2e/server/e2e.server.tunnel.js
+++ test/specs/e2e/server/e2e.server.tunnel.js
@@ -6,30 +6,54 @@ var assert = require("chai").assert;
 
 describe("Tunnel e2e tests", function () {
 
-    describe("E2E server test with tunnel", function () {
+    var instance;
 
-        var instance;
+    before(function (done) {
+        browserSync.reset();
+        var config = {
+            server:    {
+                baseDir: "test/fixtures"
+            },
+            logLevel: "silent",
+            open:      false,
+            tunnel:    true,
+            online:    true
+        };
+        instance = browserSync(config, done).instance;
+    });
 
-        before(function (done) {
-            browserSync.reset();
-            var config = {
-                server:    {
-                    baseDir: "test/fixtures"
-                },
-                logLevel: "silent",
-                open:      false,
-                tunnel:    true,
-                online:    true
-            };
-            instance = browserSync(config, done).instance;
-        });
+    after(function () {
+        instance.cleanup();
+    });
 
-        after(function () {
-            instance.cleanup();
-        });
+    it("should call init on the tunnel", function () {
+        assert.include(instance.options.getIn(["urls", "tunnel"]), "localtunnel.me");
+    });
+});
 
-        it("should call init on the tunnel", function () {
-            assert.include(instance.options.getIn(["urls", "tunnel"]), "localtunnel.me");
-        });
+describe("Tunnel e2e tests with subdomain", function () {
+
+    var instance;
+
+    before(function (done) {
+        browserSync.reset();
+        var config = {
+            server:    {
+                baseDir: "test/fixtures"
+            },
+            logLevel: "silent",
+            open:      false,
+            tunnel:    Math.floor(Math.random() * 2e10),
+            online:    true
+        };
+        instance = browserSync(config, done).instance;
+    });
+
+    after(function () {
+        instance.cleanup();
+    });
+
+    it("should call init on the tunnel", function () {
+        assert.include(instance.options.getIn(["urls", "tunnel"]), "localtunnel.me");
     });
 });
