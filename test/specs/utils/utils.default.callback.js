6f76a4458df63c3c4ee3e82a73106cc907ec07d8
Shane Osbourne <shane.osbourne8@gmail.com>
2015-03-01 22:18:08 +0000

    tests(utils): test that default callback logs errors to console

---
+++ test/specs/utils/utils.default.callback.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var utils  = require("../../../lib/utils");
+var assert = require("chai").assert;
+
+describe("Utils: default callback", function () {
+    it("should log errors if no callback given", function () {
+        var stub = require("sinon").stub(console, "error");
+        utils.defaultCallback(new Error("some error"));
+        require("sinon").assert.called(stub);
+        assert.equal(stub.getCall(0).args[0], "some error");
+        console.error.restore();
+    });
+});
