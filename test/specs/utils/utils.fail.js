545f3457977ac8aa3805c043c58cefb4522046da
Shane Osbourne <shane.osbourne8@gmail.com>
2015-03-01 21:59:28 +0000

    tests(errors): test failures with Error || string

---
+++ test/specs/utils/utils.fail.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var utils = require("../../../lib/utils");
+var assert = require("chai").assert;
+
+describe("Utils: Failing", function () {
+    it("should fail when Error obj given", function () {
+        require("sinon").stub(process, "exit");
+        utils.fail(true, new Error("Some err bro"), function (out) {
+            assert.instanceOf(out, Error);
+        });
+        process.exit.restore();
+    });
+    it("should fail with Error obj when string given", function () {
+        require("sinon").stub(process, "exit");
+        utils.fail(true, "Some err bro", function (out) {
+            assert.instanceOf(out, Error);
+        });
+        process.exit.restore();
+    });
+});
