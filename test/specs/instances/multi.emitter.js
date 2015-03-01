b4b6837f7b81cf0b8d0461f1b34bfdb81c80cba8
Shane Osbourne <shane.osbourne8@gmail.com>
2015-03-01 15:25:08 +0000

    test(emitter): ensure safe access to emitter

---
+++ test/specs/instances/multi.emitter.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var browserSync = require("../../../");
+
+var assert  = require("chai").assert;
+
+describe("E2E multi instance emitter access", function () {
+
+    it("has access to the singleton emitter", function (done) {
+
+        browserSync.reset();
+
+        browserSync({logLevel: "silent"}, function () {
+            assert.doesNotThrow(function () {
+                browserSync.emitter.emit("some:event");
+                done();
+            });
+        });
+    });
+});
