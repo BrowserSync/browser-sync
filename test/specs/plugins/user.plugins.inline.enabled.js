f177a628d6677927dffd48e1a65b01a61d61d0b9
Shane Osbourne <shane@wearejh.com>
2015-03-17 13:57:45 +0000

    deps: bump eazy-extender

---
+++ test/specs/plugins/user.plugins.inline.enabled.js
@@ -0,0 +1,63 @@
+"use strict";
+
+var browserSync = require("../../../");
+
+var assert  = require("chai").assert;
+
+describe("Plugins: Setting the default state (false) if given in options", function () {
+
+    var instance;
+
+    before(function (done) {
+
+        browserSync.reset();
+
+        var config = {
+            logLevel: "silent",
+            plugins: [{
+                "bs-html-injector": {
+                    enabled: false
+                }
+            }]
+        };
+
+        instance = browserSync(config, done).instance;
+    });
+    after(function () {
+        instance.cleanup();
+    });
+    it("Should auto disable a plugin when options given", function (done) {
+        assert.equal(instance.getUserPlugins().length, 1);
+        assert.isFalse(instance.getUserPlugins()[0].active);
+        done();
+    });
+});
+
+describe("Plugins: Setting the default state (true) if given in options", function () {
+
+    var instance;
+
+    before(function (done) {
+
+        browserSync.reset();
+
+        var config = {
+            logLevel: "silent",
+            plugins: [{
+                "bs-html-injector": {
+                    enabled: true
+                }
+            }]
+        };
+
+        instance = browserSync(config, done).instance;
+    });
+    after(function () {
+        instance.cleanup();
+    });
+    it("Should auto disable a plugin when options given", function (done) {
+        assert.equal(instance.getUserPlugins().length, 1);
+        assert.isTrue(instance.getUserPlugins()[0].active);
+        done();
+    });
+});
