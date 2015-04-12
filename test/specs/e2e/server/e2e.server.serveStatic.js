34816a76b084f8e009d1129d3a22d51061bac283
Shane Osbourne <shane.osbourne8@gmail.com>
2015-04-12 13:12:56 +0100

    fix(server): set index correctly if serveStaticOptions: {index: <path>} given

---
+++ test/specs/e2e/server/e2e.server.serveStatic.js
@@ -0,0 +1,67 @@
+"use strict";
+
+var browserSync = require("../../../../index");
+var request = require("supertest");
+var assert  = require("chai").assert;
+
+describe("E2E server test with serve static options", function () {
+
+    it("sets the index of serve-static", function (done) {
+
+        browserSync.reset();
+
+        var config = {
+            server: {
+                baseDir: "test/fixtures",
+                serveStaticOptions: {
+                    index: "inputs.html"
+                }
+            },
+            logLevel: "silent",
+            open: false
+        };
+
+        browserSync.create().init(config, function (err, bs) {
+            assert.equal(bs.options.getIn(["server", "serveStaticOptions", "index"]), "inputs.html");
+            request(bs.server)
+                .get("/")
+                .expect(200)
+                .end(function (err, res) {
+                    assert.deepEqual(
+                        require("fs").readFileSync("test/fixtures/inputs.html", "utf-8"),
+                        res.text
+                    );
+                    bs.cleanup();
+                    done();
+                });
+        });
+    });
+    it("sets uses the default for serve static index", function (done) {
+
+        browserSync.reset();
+
+        var config = {
+            server: {
+                baseDir: "test/fixtures",
+                serveStaticOptions: {}
+            },
+            logLevel: "silent",
+            open: false
+        };
+
+        browserSync.create().init(config, function (err, bs) {
+            assert.equal(bs.options.getIn(["server", "serveStaticOptions", "index"]), "index.html");
+            request(bs.server)
+                .get("/")
+                .expect(200)
+                .end(function (err, res) {
+                    assert.deepEqual(
+                        require("fs").readFileSync("test/fixtures/index.html", "utf-8"),
+                        res.text
+                    );
+                    bs.cleanup();
+                    done();
+                });
+        });
+    });
+});
