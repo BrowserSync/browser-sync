2b8f3137e56e3670e13c39c47875fa213f0cf6de
Shane Osbourne <shane@wearejh.com>
2015-06-12 13:37:40 +0100

    fix(callbacks): Bind public instance to callbacks - fixes #674

--- test/specs/e2e/e2e.options.callbacks.js
+++ test/specs/e2e/e2e.options.callbacks.js
@@ -1,10 +1,11 @@
 "use strict";
 
 var browserSync = require("../../../");
+var assert      = require("chai").assert;
 
 describe("E2E options test - ready callback as option", function () {
 
-    it("Sets the available port", function (done) {
+    it("Calls the ready callback when read", function (done) {
         browserSync.reset();
         browserSync({
             server:   {
@@ -14,6 +15,26 @@ describe("E2E options test - ready callback as option", function () {
             logLevel: "silent",
             callbacks: {
                 ready: function (err, bs) {
+                    bs.cleanup();
+                    done();
+                }
+            }
+        });
+    });
+    it("It has public instance bound to `this`", function (done) {
+        browserSync.reset();
+        browserSync({
+            server:   {
+                baseDir: "test/fixtures"
+            },
+            open:     false,
+            logLevel: "silent",
+            callbacks: {
+                ready: function () {
+                    var bs = this;
+                    assert.isFunction(bs.reload);
+                    assert.isFunction(bs.notify);
+                    assert.isFunction(bs.watch);
                     bs.cleanup();
                     done();
                 }
