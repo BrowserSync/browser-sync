c32bec6d464e7b1b52534233bfbf8ba885c4f295
Shane Osbourne <shane@wearejh.com>
2015-06-17 11:10:58 +0100

    fix(snippet): Allow async attribute to be removed from snippet with snippetOptions.async = false - fixes #670

---
+++ test/specs/e2e/e2e.options.script.async.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var browserSync = require("../../../");
+
+var assert = require("chai").assert;
+
+describe("E2E script with/without async attribute", function () {
+
+    it("serves with async", function (done) {
+        browserSync.reset();
+        var config = {open: false};
+        browserSync(config, function (err, bs) {
+            assert.include(bs.options.get("snippet"), "async");
+            bs.cleanup();
+            done();
+        });
+    });
+    it("serves without async", function (done) {
+        browserSync.reset();
+        var config = {
+            open: false,
+            snippetOptions: {
+                async: false
+            }
+        };
+        browserSync(config, function (err, bs) {
+            assert.notInclude(bs.options.get("snippet"), "async");
+            bs.cleanup();
+            done();
+        });
+    });
+});
