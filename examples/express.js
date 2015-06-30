65bb4927c7831924e29f86d4be8975c2c91a8fdb
Shane Osbourne <shane.osbourne8@gmail.com>
2015-06-30 21:11:32 +0100

    docs: add express example

---
+++ examples/express.js
@@ -0,0 +1,45 @@
+/**
+ *
+ * Install:
+ *      npm install browser-sync express
+ *
+ * Run:
+ *      node <yourfile.js>
+ *
+ * This example will create a server & use the `app` directory as the root
+ *  + use any custom routes you have registered with Express
+ *
+ */
+
+"use strict";
+
+var bs      = require("browser-sync").create();
+var express = require("express");
+var router  = express.Router();
+var app     = express();
+
+/**
+ * Catch a route like /user/2324
+ * and send a JSON response
+ */
+router.get("/user/:id", function (req, res) {
+    res.send({
+        name: "shane",
+        pets: ["cat", "hippo"],
+        id:   req.params.id
+    });
+});
+
+/**
+ * Register the route with Express
+ */
+app.use(router);
+
+/**
+ * Start the Browsersync server and
+ * load the express app as middleware
+ */
+bs.init({
+    server: "./app",
+    middleware: [app]
+});
