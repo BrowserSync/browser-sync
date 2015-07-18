0e8d0a79ba451003cbed311c63ecaa6eea5055e6
Shane Osbourne <shane.osbourne8@gmail.com>
2015-07-18 22:10:50 +0100

    docs: add proxy.proxyRes example

---
+++ examples/proxy.proxyRes.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * Install:
+ *      npm install browser-sync
+ *
+ * Run:
+ *      node <yourfile.js>
+ *
+ * This example will proxy http://www.bbc.co.uk and
+ * add headers to the response *after* it's returned from
+ * the server.
+ */
+
+"use strict";
+
+var browserSync = require("browser-sync").create();
+
+browserSync.init({
+    proxy: {
+        target: "http://www.bbc.co.uk",
+        proxyRes: [
+            function (res) {
+                res.headers["cache-control"] = "private";
+            }
+        ]
+    }
+});
