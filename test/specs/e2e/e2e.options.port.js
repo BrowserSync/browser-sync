3f211c04051f87ad1657355a3efb8200b565027d
Shane Osbourne <shane.osbourne8@gmail.com>
2015-07-26 22:46:15 +0100

    Make socketPort a sub-property of socket (so, socket.port)

--- test/specs/e2e/e2e.options.port.js
+++ test/specs/e2e/e2e.options.port.js
@@ -27,4 +27,55 @@ describe("E2E `port` option", function () {
             done();
         });
     });
+    it("sets extra port option for socket in proxy mode", function (done) {
+        browserSync.reset();
+
+        var stub = sinon.stub(utils, "getPort");
+
+        stub.onCall(0).yields(null, 3000);
+        stub.onCall(1).yields(null, 3001);
+
+        var config = {
+            logLevel: "silent",
+            proxy:   "localhost",
+            online:   false,
+            open:     false
+        };
+
+        browserSync(config, function (err, bs) {
+            bs.cleanup();
+            assert.equal(bs.options.get("port"), 3000);
+            assert.equal(stub.getCall(1).args[0], 3001);
+            assert.equal(bs.options.getIn(["socket", "port"]), 3001);
+            utils.getPort.restore();
+            done();
+        });
+    });
+    it("uses user-given extra port option for socket in proxy mode", function (done) {
+        browserSync.reset();
+
+        var stub = sinon.stub(utils, "getPort");
+
+        stub.onCall(0).yields(null, 3000);
+        stub.onCall(1).yields(null, 8001);
+
+        var config = {
+            logLevel: "silent",
+            proxy:   "localhost",
+            socket: {
+                port: 8001
+            },
+            online:   false,
+            open:     false
+        };
+
+        browserSync(config, function (err, bs) {
+            bs.cleanup();
+            assert.equal(bs.options.get("port"), 3000);
+            assert.equal(stub.getCall(1).args[0], 8001);
+            assert.equal(bs.options.getIn(["socket", "port"]), 8001);
+            utils.getPort.restore();
+            done();
+        });
+    });
 });
