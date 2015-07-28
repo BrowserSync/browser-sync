bbba2e96f02972081088caebf03028c5a1986ac9
Shane Osbourne <shane.osbourne8@gmail.com>
2015-07-28 13:13:49 +0100

    tests: add proxy + websocket test via api usage

---
+++ test/specs/e2e/proxy/e2e.proxy.ws.js
@@ -0,0 +1,53 @@
+"use strict";
+
+var browserSync = require("../../../../index");
+var connect = require("connect");
+var assert = require("chai").assert;
+var socket = require("socket.io");
+var client = require("socket.io-client");
+
+describe("E2E proxy + Web sockets test", function () {
+
+    var instance, server, options;
+
+    before(function (done) {
+
+        browserSync.reset();
+
+        var app = connect();
+        server = app.listen();
+        var proxytarget = "http://localhost:" + server.address().port;
+
+        var config = {
+            proxy: {
+                target: proxytarget,
+                ws: true
+            },
+            logLevel: "silent",
+            open: false
+        };
+
+        instance = browserSync.init([], config, function (err, bs) {
+            options = bs.options;
+            done();
+        }).instance;
+    });
+
+    after(function () {
+        instance.cleanup();
+        server.close();
+    });
+
+    it("can init proxy & serve a page", function (done) {
+
+        assert.equal(instance.options.getIn(["proxy", "ws"]), true);
+
+        socket(server);
+
+        server.on("upgrade", function () {
+            done();
+        });
+
+        client.connect(instance.options.getIn(["urls", "local"]), {forceNew: true});
+    });
+});
