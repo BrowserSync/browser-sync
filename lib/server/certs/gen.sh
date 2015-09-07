2eff3141ce8c816aa928706e6394c60fb071df2d
Juergen Zimmermann <Juergen.Zimmermann (at) HS-Karlsruhe.de>
2015-09-07 07:06:35 +0200

    Issue 754: cert with SHA512

--- lib/server/certs/gen.sh
+++ lib/server/certs/gen.sh
@@ -1,6 +1,6 @@
-openssl genrsa -des3 -out server.key 1024
+openssl genrsa -des3 -out server.key 8192
 openssl req -new -key server.key -out server.csr
-openssl x509 -req -days 3650 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -days 3650 -sha512 -in server.csr -signkey server.key -out server.crt -subj "/CN=localhost/O=Internet Widgits Pty Ltd/S=Some-State/C=AU"
 cp server.key server.key.copy
 openssl rsa -in server.key.copy -out server.key
 rm server.key.copy
