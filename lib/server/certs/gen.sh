2950aaa4653afd742203f22df8108a13c5deac9b
Juergen Zimmermann <Juergen.Zimmermann (at) HS-Karlsruhe.de>
2015-09-07 11:01:50 +0200

    Issue 754: cert with SHA512 (Take 2)

--- lib/server/certs/gen.sh
+++ lib/server/certs/gen.sh
@@ -1,6 +1,9 @@
-openssl genrsa -des3 -out server.key 8192
-openssl req -new -key server.key -out server.csr
-openssl x509 -req -days 3650 -sha512 -in server.csr -signkey server.key -out server.crt -subj "/CN=localhost/O=Internet Widgits Pty Ltd/S=Some-State/C=AU"
-cp server.key server.key.copy
-openssl rsa -in server.key.copy -out server.key
-rm server.key.copy
+openssl genrsa -out server.key 8192
+openssl req -new -x509 -sha512 -key server.key -out server.crt -days 3650 -subj "/CN=localhost/O=Internet Widgits Pty Ltd/ST=Some-State/C=AU"
+
+# openssl genrsa -des3 -out server.key 8192
+# openssl req -new -key server.key -out server.csr
+# openssl x509 -req -days 3650 -sha512 -in server.csr -signkey server.key -out server.crt -subj "/CN=localhost/O=Internet Widgits Pty Ltd/S=Some-State/C=AU"
+# cp server.key server.key.copy
+# openssl rsa -in server.key.copy -out server.key
+# rm server.key.copy
