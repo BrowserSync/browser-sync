45fc26f7075369b329bc6e974a650b529b107c61
Shane Osbourne <shane.osbourne8@gmail.com>
2015-10-22 20:45:42 +0100

    Add 404 example

---
+++ examples/404.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Install:
+ *      npm install browser-sync
+ *
+ * Run:
+ *      node <yourfile.js>
+ *
+ * This example will redirect all 404 requests to a
+ * custom 404.html page
+ *
+ */
+
+"use strict";
+
+var browserSync = require("browser-sync").create();
+
+browserSync.init({
+    files: ["app/css/*.css"],
+    server: {
+        baseDir: "app"
+    }
+}, function (err, bs) {
+    bs.addMiddleware("*", function (req, res) {
+        res.writeHead(302, {
+            "location": "404.html"
+        });
+        res.end('Redirecting!');
+    });
+});
