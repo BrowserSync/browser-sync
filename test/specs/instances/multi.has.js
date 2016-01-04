5096834d4b5cfad529d1283443977f2ce7acee29
Shane Osbourne <shane.osbourne8@gmail.com>
2016-01-04 09:38:38 +0000

    tests: add tests for API.has(name)

---
+++ test/specs/instances/multi.has.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var browserSync = require("../../../");
+
+var assert  = require("chai").assert;
+
+describe("E2E multi instance has", function () {
+
+    it("returns true if an instance exists", function () {
+        browserSync.reset();
+        browserSync.create("ts");
+        assert.isTrue(browserSync.has("ts"));
+    });
+    it("returns false if an instance does not exist", function () {
+        browserSync.reset();
+        assert.isFalse(browserSync.has("ts"));
+        browserSync.create("ts2");
+        assert.isTrue(browserSync.has("ts2"));
+        assert.isFalse(browserSync.has("ts"));
+    });
+});
