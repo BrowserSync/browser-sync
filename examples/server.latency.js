41ebcfc6f1dd5218c61b08cd05949e8deab9b559
Shane Osbourne <shane.osbourne8@gmail.com>
2016-02-08 15:47:55 +0000

    docs: add fake latency example

---
+++ examples/server.latency.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * Install:
+ *      npm install browser-sync
+ *
+ * Run:
+ *      node <yourfile.js>
+ *
+ * This example will create a server & use the `app` directory as the root
+ *  - any requests begining with /json will have fake latency applied
+ *  - for 3 seconds
+ *
+ */
+
+"use strict";
+
+var browserSync = require("browser-sync").create();
+
+function fakeLatency (req, res, next) {
+    if (req.url.match(/^\/json/)) {
+        setTimeout(next, 3000);
+    } else {
+        next();
+    }
+}
+
+browserSync.init({
+    files: ["app/css/*.css"],
+    server: "app",
+    middleware: [fakeLatency]
+});
