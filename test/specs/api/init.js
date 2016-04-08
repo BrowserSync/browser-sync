f09a3edde84b0a14a182c24112d210c0ee6a7352
Shane Osbourne <shane.osbourne8@gmail.com>
2016-04-08 21:08:52 +0100

    tests: add tests for init with null/undefined

--- test/specs/api/init.js
+++ test/specs/api/init.js
@@ -6,29 +6,43 @@ var assert      = require("chai").assert;
 
 describe("API: .init - don't not call init when already running.", function () {
 
-    var instance;
-
-    before(function (done) {
+    it("should know the active State of BrowserSync", function (done) {
         browserSync.reset();
-        var config = {
+        browserSync({
             logLevel: "silent",
             open: false
-        };
-
-        instance = browserSync(config, function () {
+        }, function (err, bs) {
+            var spy = require("sinon").spy(console, "log");
+            browserSync({server: "test/fixtures"});
+            var arg = spy.getCall(0).args[0];
+            assert.include(arg, "browserSync.create().init()");
+            console.log.restore();
+            bs.cleanup();
             done();
         });
     });
-
-    after(function () {
-        instance.cleanup();
+    it("should init with null as second param", function (done) {
+        browserSync.reset();
+        var bs = browserSync({
+            logLevel: "silent",
+            open: false
+        }, null);
+        bs.emitter.on("service:running", function () {
+            assert.ok(bs.instance.options.get("urls"));
+            bs.cleanup();
+            done();
+        });
     });
-
-    it("should know the active State of BrowserSync", function () {
-        var spy = require("sinon").spy(console, "log");
-        browserSync({server: "test/fixtures"});
-        var arg = spy.getCall(0).args[0];
-        assert.include(arg, "browserSync.create().init()");
-        console.log.restore();
+    it("should init with undefined as second param", function (done) {
+        browserSync.reset();
+        var bs = browserSync({
+            logLevel: "silent",
+            open: false
+        }, undefined);
+        bs.emitter.on("service:running", function () {
+            assert.ok(bs.instance.options.get("urls"));
+            bs.cleanup();
+            done();
+        });
     });
 });
