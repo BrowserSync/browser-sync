7a1e42e99c760176e8ad9bdd7b4d31305a1af9d8
Shane Osbourne <shane.osbourne8@gmail.com>
2016-04-09 10:55:41 +0100

    tests: add initial rewriteRule to ensure the filtering works as expected

--- test/specs/e2e/server/e2e.server.rewrite.rules.remove.js
+++ test/specs/e2e/server/e2e.server.rewrite.rules.remove.js
@@ -7,55 +7,45 @@ var assert  = require("chai").assert;
 
 describe("E2E server test with rewrite rules removed on the fly", function () {
 
-    var bs;
-
-    before(function (done) {
+    it("serves files with HTML rewritten", function (done) {
 
         browserSync.reset();
-
-        var config = {
+        browserSync.init({
             server: {
                 baseDir: "test/fixtures"
             },
+            rewriteRules: [{match: "kittie", replace: "shane"}],
             logLevel: "silent",
             open: false
-        };
+        }, function (err, bs) {
 
-        bs = browserSync.init(config, done).instance;
-    });
-
-    after(function () {
-        bs.cleanup();
-    });
-
-    it("serves files with HTML rewritten", function (done) {
-
-        bs.addRewriteRule({
-            id: "myrule",
-            match: /Forms/g,
-            fn: function () {
-                return "Shane's forms";
-            }
-        });
-
-        request(bs.server)
-            .get("/index.html")
-            .set("accept", "text/html")
-            .expect(200)
-            .end(function (err, res) {
-                assert.include(res.text, "Shane's forms");
-
-                bs.removeRewriteRule("myrule");
-
-                request(bs.server)
-                    .get("/index.html")
-                    .set("accept", "text/html")
-                    .expect(200)
-                    .end(function (err, res) {
-                        assert.notInclude(res.text, "Shane's forms");
-                        done();
-                    });
-
+            bs.addRewriteRule({
+                id: "myrule",
+                match: /Forms/g,
+                fn: function () {
+                    return "Shane's forms";
+                }
             });
+
+            request(bs.server)
+                .get("/index.html")
+                .set("accept", "text/html")
+                .expect(200)
+                .end(function (err, res) {
+                    assert.include(res.text, "Shane's forms");
+
+                    bs.removeRewriteRule("myrule");
+
+                    request(bs.server)
+                        .get("/index.html")
+                        .set("accept", "text/html")
+                        .expect(200)
+                        .end(function (err, res) {
+                            assert.notInclude(res.text, "Shane's forms");
+                            done();
+                        });
+
+                });
+        });
     });
 });
