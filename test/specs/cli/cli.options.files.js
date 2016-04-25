5333848e9f93a86a076780283c84fefa6e5c1e4b
Shane Osbourne <shane.osbourne8@gmail.com>
2016-04-25 09:28:33 +0100

    fix(files): Fix regression where strings from the CLI files option are no-longer exploded if they enter as an array - fixes  #1076

--- test/specs/cli/cli.options.files.js
+++ test/specs/cli/cli.options.files.js
@@ -13,6 +13,13 @@ describe("CLI: Options: Merging Options: Files", function () {
             objs: []
         });
     });
+    it("should return the files property from Array given with strings in legacy format", function () {
+        var imm = merge({files: ["css/*.css,*.html"]});
+        assert.deepEqual(imm.get("files").get("core").toJS(), {
+            globs: ["css/*.css", "*.html"],
+            objs: []
+        });
+    });
     it("should return the files property from array given", function () {
         var imm = merge({files: ["css/*.css", "*.html"]});
         assert.deepEqual(imm.get("files").get("core").toJS(), {
