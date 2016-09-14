62d83b895d032624957474b6a23da157913caabd
Shane Osbourne <shane.osbourne8@gmail.com>
2016-09-14 21:07:33 +0100

    feat(cors): Add new option 'cors' for adding HTTP access control (CORS) to assets served by Browsersync

---
+++ test/specs/e2e/e2e.options.cors.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var browserSync = require("../../../");
+
+var assert = require("chai").assert;
+var sinon = require("sinon");
+var request     = require("supertest");
+
+describe("e2e options test (cors)", function () {
+    it("Adds cors middleware", function (done) {
+        browserSync.reset();
+        var config = {
+            server: {
+                baseDir: "test/fixtures"
+            },
+            open: false,
+            logLevel: "silent",
+            cors: true
+        };
+        browserSync(config, function (err, bs) {
+            request(bs.server)
+                .get("/index.html")
+                .expect(200)
+                .end(function (err, res) {
+                    assert.equal(res.headers['access-control-allow-origin'], '*');
+                    assert.equal(res.headers['access-control-allow-methods'], 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+                    assert.equal(res.headers['access-control-allow-headers'], 'X-Requested-With,content-type');
+                    assert.equal(res.headers['access-control-allow-credentials'], 'true');
+                    bs.cleanup();
+                    done();
+                });
+        });
+    });
+});
