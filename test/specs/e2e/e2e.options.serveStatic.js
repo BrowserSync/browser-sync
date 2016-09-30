da4943185341daa420fadc856d95a3755a908620
Shane Osbourne <shane.osbourne8@gmail.com>
2016-09-30 08:43:48 +0100

    tests for new serveStatic options

--- test/specs/e2e/e2e.options.serveStatic.js
+++ test/specs/e2e/e2e.options.serveStatic.js
@@ -3,34 +3,98 @@
 var browserSync = require("../../../index");
 
 var request     = require("supertest");
-var assert      = require("chai").assert;
 var page        = require("fs").readFileSync("test/fixtures/index.html", "utf-8");
+var css         = require("fs").readFileSync("test/fixtures/assets/style.css", "utf-8");
+var Rx          = require('rx');
+
+function getRequests (reqs, server) {
+    return reqs.map(function (req) {
+        return Rx.Observable.create(function (obs) {
+            request(server)
+                .get(req[0])
+                .end(function (err, res) {
+                    if (err) {
+                        return obs.onError(err);
+                    }
+                    if (res.text === req[1]) {
+                        obs.onNext(true);
+                        obs.onCompleted();
+                        return;
+                    }
+
+                    obs.onError(new Error(req[0] + " did not match the response body"));
+                });
+        });
+    });
+}
 
 describe("E2E `serveStatic` option", function () {
 
-    var bs;
-
-    before(function (done) {
+    it("can serve static files with string only", function (done) {
         browserSync.reset();
         var config = {
             logLevel: "silent",
             online:    false,
             serveStatic: ["test/fixtures"]
         };
-        bs = browserSync(config, done).instance;
-    });
 
-    after(function () {
-        bs.cleanup();
+        browserSync(config, function (err, bs) {
+            request(bs.server)
+                .get("/index.html")
+                .expect(200, page, function () {
+                    bs.cleanup();
+                    done();
+                });
+        });
     });
-
-    it("can serve static files regardless of running mode", function (done) {
-        request(bs.server)
-            .get("/index.html")
-            .expect(200)
-            .end(function (err, res) {
-                assert.equal(res.text, page);
-                done();
-            });
+    it("can serve static files with string multiple string", function (done) {
+        browserSync.reset();
+        var config = {
+            logLevel: "silent",
+            online:    false,
+            serveStatic: ["test/fixtures", "test/fixtures/assets"]
+        };
+        browserSync(config, function (err, bs) {
+            var reqs = getRequests([["/index.html", page], ["/style.css", css]], bs.server);
+            var obs = Rx.Observable.merge(reqs);
+            obs.subscribeOnCompleted(done);
+        });
+    });
+    it("can serve static files with multiple objects", function (done) {
+        browserSync.reset();
+        var config = {
+            logLevel: "silent",
+            online:    false,
+            serveStatic: [
+                {route: "",   dir: "test/fixtures"},
+                {route: [""], dir: "test/fixtures/assets"}
+            ]
+        };
+        browserSync(config, function (err, bs) {
+            var reqs = getRequests([['/index.html', page], ['/style.css', css]], bs.server);
+            var obs = Rx.Observable.merge(reqs);
+            obs.subscribeOnCompleted(done);
+        });
+    });
+    it("can serve static files with multiple roots", function (done) {
+        browserSync.reset();
+        var config = {
+            logLevel: "silent",
+            online:    false,
+            serveStatic: [
+                {route: "",   dir: "test/fixtures"},
+                {route: ["/shane", "/kittie"], dir: "test/fixtures/assets"},
+                {route: "", dir: "test/fixtures/assets"}
+            ]
+        };
+        browserSync(config, function (err, bs) {
+            var reqs = getRequests([
+                ['/index.html', page],
+                ['/shane/style.css', css],
+                ['/kittie/style.css', css]
+            ], bs.server);
+            var obs = Rx.Observable.merge(reqs);
+            obs.subscribeOnCompleted(done);
+        });
     });
 });
