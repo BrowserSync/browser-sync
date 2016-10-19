d022c1d7462b377cef9bf5b64aa2ab36aee1de1a
Shane Osbourne <shane.osbourne8@gmail.com>
2016-10-19 07:40:28 +0100

    fix(extensions): Ensure 'extensions' options is respected when array received - fixes #1220

---
+++ test/specs/e2e/server/e2e.server.serveStatic.extensions.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var browserSync = require("../../../../index");
+var assert  = require("chai").assert;
+var utils   = require("../../../utils");
+var Rx      = require("rx");
+
+describe("E2E server test with extension option", function () {
+
+    it("serves using 'extensions'", function (done) {
+
+        browserSync.reset();
+
+        var config = {
+            server: {
+                baseDir: "test/fixtures"
+            },
+            extensions: ['html'],
+            snippetOptions: {
+                blacklist: ['/forms']
+            },
+            logLevel: "silent",
+            open: false
+        };
+
+        browserSync.create().init(config, function (err, bs) {
+            assert.equal(bs.options.getIn(["server", "serveStaticOptions", "extensions", 0]), "html");
+            var reqs = utils.getRequests([
+                ["/forms", require("fs").readFileSync("test/fixtures/forms.html", "utf-8")]
+            ], bs.server);
+            Rx.Observable.concat(reqs).subscribeOnCompleted(function () {
+                bs.cleanup();
+                done();
+            });
+        });
+    });
+});
