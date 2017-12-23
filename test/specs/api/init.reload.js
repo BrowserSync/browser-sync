a62b0df7b655e0e549f35b872650581f275ef4f1
Shane Osbourne <shane.osbourne8@gmail.com>
2017-12-23 20:04:08 +0000

    tests: update .reload() api tests

--- test/specs/api/init.reload.js
+++ test/specs/api/init.reload.js
@@ -4,105 +4,171 @@ var sinon = require("sinon");
 var assert = require("chai").assert;
 var File = require("vinyl");
 
-describe.only("API: .reload()", function() {
-    var emitterStub, clock, bs, scheduler;
-
-    before(function(done) {
+describe("API: .reload()", function() {
+    it("should accept a file path as a string", function(done) {
         browserSync.reset();
-        scheduler = require("../../utils").getScheduler();
-        bs = browserSync({
-            logLevel: "silent",
-            debug: {
-                scheduler: scheduler
+
+        var scheduler = require("../../utils").getScheduler();
+
+        browserSync(
+            {
+                logLevel: "silent",
+                debug: {
+                    scheduler: scheduler
+                }
+            },
+            function(err, bs) {
+                var emitterStub = sinon.spy(bs.emitter, "emit");
+
+                browserSync.reload("css/core.css");
+                scheduler.advanceTo(500);
+
+                sinon.assert.calledWithExactly(emitterStub, "file:changed", {
+                    path: "css/core.css",
+                    basename: "core.css",
+                    log: true,
+                    namespace: "core",
+                    event: "change",
+                    ext: "css"
+                });
+
+                bs.cleanup();
+
+                done();
             }
-        }, function() {
-            emitterStub = sinon.spy(bs.emitter, "emit");
-            done();
-        });
-        clock = sinon.useFakeTimers();
+        );
     });
+    it("only calls reload once if the array contains a filepath that will cause a reload", function(done) {
+        browserSync.reset();
 
-    afterEach(function() {
-        emitterStub.reset();
-        clock.now = 0;
-    });
+        var scheduler = require("../../utils").getScheduler();
 
-    after(function() {
-        bs.cleanup();
-        clock.restore();
-        emitterStub.restore();
-    });
+        browserSync(
+            {
+                logLevel: "silent",
+                debug: {
+                    scheduler: scheduler
+                }
+            },
+            function(err, bs) {
+                var emitterStub = sinon.spy(bs.emitter, "emit");
 
-    it("should be callable with no args & perform a reload", function() {
-        browserSync.reload();
-        sinon.assert.calledWithExactly(emitterStub, "_browser:reload");
+                browserSync.reload(["css/core.css", "index.html"]);
+                scheduler.advanceTo(2000);
+
+                var calls = emitterStub.withArgs("browser:reload");
+                assert.equal(calls.callCount, 1);
+                sinon.assert.calledWith(emitterStub, "browser:reload");
+
+                bs.cleanup();
+                done();
+            }
+        );
     });
-    it("should accept a file path as a string", function() {
-        browserSync.reload("css/core.css");
-        scheduler.advanceTo(501);
-        sinon.assert.calledWithExactly(emitterStub, "file:changed", {
-            path: "css/core.css",
-            basename: "core.css",
-            log: true,
-            namespace: "core",
-            event: "change",
-            ext: "css"
-        });
+    it("calls reload multiple times if all items can be injected", function(done) {
+        browserSync.reset();
+
+        var scheduler = require("../../utils").getScheduler();
+
+        browserSync(
+            {
+                logLevel: "silent",
+                debug: {
+                    scheduler: scheduler
+                }
+            },
+            function(err, bs) {
+                var emitterStub = sinon.spy(bs.emitter, "emit");
+
+                browserSync.reload(["css/core.css", "ie.css"]);
+                scheduler.advanceTo(2000);
+
+                var calls = emitterStub.withArgs("file:changed");
+                assert.equal(calls.callCount, 2);
+
+                sinon.assert.calledWithExactly(emitterStub, "file:changed", {
+                    path: "css/core.css",
+                    basename: "core.css",
+                    log: true,
+                    namespace: "core",
+                    event: "change",
+                    ext: "css"
+                });
+                sinon.assert.calledWithExactly(emitterStub, "file:changed", {
+                    path: "ie.css",
+                    basename: "ie.css",
+                    log: true,
+                    namespace: "core",
+                    event: "change",
+                    ext: "css"
+                });
+
+                bs.cleanup();
+                done();
+            }
+        );
     });
-    it("only calls reload once if the array contains a filepath that will cause a reload", function() {
-        browserSync.reload(["css/core.css", "index.html"]);
-        var calls = emitterStub.withArgs("browser:reload");
-        assert.equal(calls.callCount, 1);
-        sinon.assert.calledWithExactly(emitterStub, "browser:reload");
-    });
-    it("calls reload multiple times if all items can be injected", function() {
-        browserSync.reload(["css/core.css", "ie.css"]);
-        var calls = emitterStub.withArgs("file:changed");
-        assert.equal(calls.callCount, 2);
-        sinon.assert.calledWithExactly(emitterStub, "file:changed", {
-            path: "css/core.css",
-            basename: "core.css",
-            log: true,
-            namespace: "core",
-            event: "change",
-            ext: "css"
-        });
-        sinon.assert.calledWithExactly(emitterStub, "file:changed", {
-            path: "ie.css",
-            basename: "ie.css",
-            log: true,
-            namespace: "core",
-            event: "change",
-            ext: "css"
-        });
-    });
-    it("should accept an array of file paths as strings", function() {
-        browserSync.reload(["index.html", "css/core.css"]);
-        sinon.assert.calledWithExactly(emitterStub, "browser:reload");
-    });
-    it("should accept wildcards for files extensions eg: *.css", function() {
-        browserSync.reload("*.css");
-        sinon.assert.calledWithExactly(emitterStub, "file:changed", {
-            path: "*.css",
-            basename: "*.css",
-            log: true,
-            namespace: "core",
-            event: "change",
-            ext: "css"
-        });
+    it("should accept wildcards for files extensions eg: *.css", function(done) {
+        browserSync.reset();
+
+        var scheduler = require("../../utils").getScheduler();
+
+        browserSync(
+            {
+                logLevel: "silent",
+                debug: {
+                    scheduler: scheduler
+                }
+            },
+            function(err, bs) {
+                var emitterStub = sinon.spy(bs.emitter, "emit");
+
+                browserSync.reload("*.css");
+
+                scheduler.advanceTo(500);
+
+                sinon.assert.calledWithExactly(emitterStub, "file:changed", {
+                    path: "*.css",
+                    basename: "*.css",
+                    log: true,
+                    namespace: "core",
+                    event: "change",
+                    ext: "css"
+                });
+
+                bs.cleanup(done);
+            }
+        );
     });
     /**
      * BACKWARDS COMPATIBILITY:
      * This is an old signature that, whilst we must continue to support,
      * is now deferred to the stream method.
      */
-    it("should reload browser if once:true given as arg", function() {
-        var stream = browserSync.reload({ stream: true, once: true });
-        stream.write(new File({ path: "styles.css" }));
-        stream.write(new File({ path: "styles2.css" }));
-        stream.write(new File({ path: "styles3.css" }));
-        stream.end();
-        sinon.assert.calledWithExactly(emitterStub, "_browser:reload");
-        sinon.assert.calledWithExactly(emitterStub, "browser:reload");
+    it("should reload browser if once:true given as arg", function(done) {
+        browserSync.reset();
+
+        var scheduler = require("../../utils").getScheduler();
+
+        browserSync(
+            {
+                logLevel: "silent",
+                debug: {
+                    scheduler: scheduler
+                }
+            },
+            function(err, bs) {
+                var emitterStub = sinon.spy(bs.emitter, "emit");
+                var stream = browserSync.reload({ stream: true, once: true });
+                stream.write(new File({ path: "styles.css" }));
+                stream.write(new File({ path: "styles2.css" }));
+                stream.write(new File({ path: "styles3.css" }));
+                stream.end();
+                scheduler.advanceTo(600);
+                sinon.assert.calledWithExactly(emitterStub, "_browser:reload");
+                sinon.assert.calledWithExactly(emitterStub, "browser:reload");
+                bs.cleanup(done);
+            }
+        );
     });
 });
