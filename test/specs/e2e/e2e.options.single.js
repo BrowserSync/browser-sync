91480aa692779825aef0791b79d79139e56030f4
Shane Osbourne <shane.osbourne8@gmail.com>
2017-12-26 21:32:26 +0000

    feat: add 'single' flag to put Browsersync into SPA mode

---
+++ test/specs/e2e/e2e.options.single.js
@@ -0,0 +1,54 @@
+var browserSync = require("../../../");
+
+var assert = require("chai").assert;
+var sinon = require("sinon");
+var fs = require("fs");
+var request = require("supertest");
+
+describe("e2e options test (single)", function() {
+    it("returns index.html content for a non-existing path", function(done) {
+        browserSync.reset();
+        var config = {
+            server: {
+                baseDir: "test/fixtures"
+            },
+            open: false,
+            logLevel: "silent",
+            cors: true,
+            single: true
+        };
+        browserSync(config, function(err, bs) {
+            request(bs.server)
+                .get("/a-path-that-deffo-doesnot-exist")
+                .set('accept', '*/*')
+                .expect(200)
+                .end(function(err, res) {
+                    const expected = fs.readFileSync('test/fixtures/index.html', 'utf8');
+                    assert.equal(res.text, expected);
+                    bs.cleanup(done);
+                });
+        });
+    });
+    it("returns regular content for matching file path", function(done) {
+        browserSync.reset();
+        var config = {
+            server: {
+                baseDir: "test/fixtures"
+            },
+            open: false,
+            logLevel: "silent",
+            cors: true,
+            single: true
+        };
+        browserSync(config, function(err, bs) {
+            request(bs.server)
+                .get("/assets/style.css")
+                .expect(200)
+                .end(function(err, res) {
+                    const expected = fs.readFileSync('test/fixtures/assets/style.css', 'utf8');
+                    assert.equal(res.text, expected);
+                    bs.cleanup(done);
+                });
+        });
+    });
+});
