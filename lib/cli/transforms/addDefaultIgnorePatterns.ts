dd35b50ce48c805d318ddb0ddacc3b2fd3bd5783
Shane Osbourne <shane.osbourne8@gmail.com>
2017-12-28 09:27:08 +0000

    make it clearer where default ignore patterns originate

--- lib/cli/transforms/addDefaultIgnorePatterns.ts
+++ lib/cli/transforms/addDefaultIgnorePatterns.ts
@@ -1,5 +1,14 @@
 import {List} from "immutable";
 
+const defaultIgorePatterns = [
+    /node_modules/,
+    /bower_components/,
+    /\.sass-cache/,
+    /\.vscode/,
+    /\.git/,
+    /\.idea/
+];
+
 export function addDefaultIgnorePatterns(incoming) {
     if (!incoming.get("watch")) {
         return incoming;
@@ -11,16 +20,7 @@ export function addDefaultIgnorePatterns(incoming) {
             .filter(Boolean)
             .toSet();
 
-        const merged = userIgnored.merge([
-            /node_modules/,
-            /bower_components/,
-            /\.sass-cache/,
-            /\.vscode/,
-            /\.git/,
-            /\.idea/,
-            "!node_modules/**/*",
-            "!**/node_modules/**"
-        ]);
+        const merged = userIgnored.merge(defaultIgorePatterns);
 
         return watchOptions.merge({
             ignored: merged.toList(),
