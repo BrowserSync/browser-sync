11018e6e38dc989ca584d9ab8e6e330c2683a535
Shane Osbourne <shane.osbourne8@gmail.com>
2018-01-16 11:11:28 +0000

    add example for notify styles

---
+++ examples/notify-styles.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * Install:
+ *      npm install browser-sync
+ *
+ * Run:
+ *      node <yourfile.js>
+ *
+ */
+
+"use strict";
+
+var browserSync = require("../").create();
+
+browserSync.init({
+    open: false, // Stop auto open browser
+    notify: {
+        styles: [
+            'display: none;',
+            'padding: 6px 15px 3px;',
+            'position: fixed;',
+            'font-size: 40px;',
+            'z-index: 9999;',
+            'left: 0px;',
+            'bottom: 0px;',
+            'color: rgb(74, 74, 74);',
+            'background-color: rgb(17, 17, 17);',
+            'color: rgb(229, 229, 229);'
+        ]
+    },
+    server: {
+        baseDir: 'test/fixtures'
+    }
+});
