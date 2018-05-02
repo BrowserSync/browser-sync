cc5118cf5382761f85bb34e3b40563046a3fdeb9
Shane Osbourne <shane.osbourne8@gmail.com>
2018-05-02 21:58:54 +0100

    fix: Don’t always add "defaultIgnorePatterns" - fixes #1543

---
+++ cypress/integration/file-watching-ignore.js
@@ -0,0 +1,14 @@
+describe('Watching folders', function() {
+    context('files option', function () {
+        beforeEach(function () {
+            cy.visit(Cypress.env('BS_URL') + '/bower.html');
+        });
+        it('should reload single <link>', function () {
+            cy.exec('touch test/fixtures/bower_components/app.css');
+            cy.get('[id="css-style"]').should($link => {
+                const url = new URL($link.attr('href'));
+                expect(url.search).to.contain('?browsersync=');
+            });
+        });
+    });
+});
--- lib/cli/transforms/copyCLIIgnoreToWatchOptions.ts
+++ lib/cli/transforms/copyCLIIgnoreToWatchOptions.ts
@@ -4,7 +4,7 @@ export function copyCLIIgnoreToWatchOptions(incoming) {
     if (!incoming.get("ignore")) {
         return incoming;
     }
-    return incoming.updateIn(["watchOptions", "ignored"], ignored => {
+    return incoming.updateIn(["watchOptions", "ignored"], List([]), ignored => {
         const userIgnore = List([]).concat(incoming.get("ignore"));
         return ignored.concat(userIgnore);
     });
