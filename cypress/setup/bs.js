a6d39e6e89f6a12e9a16e08b3bc71d76cc30f344
Shane Osbourne <shane.osbourne8@gmail.com>
2018-05-03 21:28:33 +0100

    fix: Remote Debug tools do not work - fixes #1556

--- cypress/setup/bs.js
+++ cypress/setup/bs.js
@@ -1,20 +1,35 @@
-module.exports = function(setup, specs) {
+const cypress = require('cypress');
+const exec = require('child_process');
+const assert = require('assert');
+const {join} = require('path');
+const {Observable} = require('rxjs/Observable');
 
-    const cypress = require('cypress');
-    const bs      = require('../../').create();
+module.exports = function(opts, ctx) {
 
-    bs.init(setup, function(err, bs) {
-        return cypress.run({
-            spec: specs,
-            env: `BS_URL=${bs.options.getIn(['urls', 'local'])}`
-        })
-            .then((results) => {
-                // stop your server when it's complete
-                bs.cleanup();
-                if (results.failures > 0) {
-                    return process.exit(1);
-                }
-                process.exit(0);
+    assert.ok(typeof opts.config === 'string', '`opts.config` should be a string');
+    assert.ok(typeof opts.spec === 'string', '`opts.spec` should be a string');
+    assert.ok((opts.action === 'run' || opts.action === 'open'), '`action` should be either run or open');
+
+    const json = require(join(ctx.config.cwd, opts.config));
+
+    return Observable.create(obs => {
+        const bs = require('../../').create();
+        const instance = bs.init(json, function(err, bs) {
+            if (err) {
+                return obs.error(err);
+            }
+            return cypress[opts.action]({
+                spec: opts.spec,
+                env: `BS_URL=${bs.options.getIn(['urls', 'local'])},BS_UI_URL=${bs.options.getIn(['urls', 'ui'])}`
             })
+                .then((results) => {
+                    // stop your server when it's complete
+                    if (results.failures > 0) {
+                        return obs.error(new Error('Errors occurred'));
+                    }
+                    instance.cleanup();
+                    obs.complete();
+                })
+        });
     });
 };
--- cypress/setup/run.js
+++
@@ -1,5 +0,0 @@
-const run = require('./bs');
-const args = process.argv.slice(2);
-const config = require(args[0]);
-const specs = args.slice(1);
-run(config, specs);
