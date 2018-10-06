06ee1b7db8d3090f693dc8ba0650f3e7022463ae
Shane Osbourne <shane.osbourne8@gmail.com>
2018-10-06 11:28:13 +0100

    fix: Unexpected Page Reload after Pausing in Debugger - fixes #1591

--- packages/browser-sync-client/lib/messages/Connection.ts
+++ packages/browser-sync-client/lib/messages/Connection.ts
@@ -6,7 +6,6 @@ import * as Log from "../log";
 import { withLatestFrom } from "rxjs/operators/withLatestFrom";
 import { mergeMap } from "rxjs/operators/mergeMap";
 import { setOptions } from "../effects/set-options.effect";
-import { reloadBrowserSafe } from "./BrowserReload";
 
 export function incomingConnection(
     xs: Observable<IBrowserSyncOptions>,
@@ -14,14 +13,11 @@ export function incomingConnection(
 ) {
     return xs.pipe(
         withLatestFrom(inputs.option$.pipe(pluck("logPrefix"))),
-        mergeMap(([x, logPrefix], index) => {
-            if (index === 0) {
-                return of<any>(
-                    setOptions(x),
-                    Log.overlayInfo(`${logPrefix}: connected`)
-                );
-            }
-            return reloadBrowserSafe();
+        mergeMap(([x, logPrefix]) => {
+            return of<any>(
+                setOptions(x),
+                Log.overlayInfo(`${logPrefix}: connected`)
+            );
         })
     );
 }
