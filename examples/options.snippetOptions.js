62d906e6abde0c251550a598e159c67d53e287a4
Shane Osbourne <shane.osbourne8@gmail.com>
2023-05-17 20:40:06 +0200

    fix: append to head if body not present yet - fixes #2031 (#2041)
    Co-authored-by: Shane Osbourne <sosbourne@duckduckgo.com>

---
+++ examples/options.snippetOptions.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Install:
+ *      npm install browser-sync
+ *
+ * Run:
+ *      node <yourfile.js>
+ *
+ * This example shows how you can place the snippet anywhere.
+ */
+
+"use strict";
+
+var path = require("path");
+var browserSync = require("../packages/browser-sync").create();
+var cwd = path.join(__dirname, "..");
+var fixtures_dir = path.join(cwd, "packages/browser-sync/test/fixtures");
+
+browserSync.init({
+    files: [path.join(fixtures_dir, "css/*.css")],
+    server: fixtures_dir,
+    snippetOptions: {
+        rule: {
+            match: /<\/head>/i,
+            fn: function (snippet, match) {
+                return snippet + match;
+            },
+        },
+    },
+});
