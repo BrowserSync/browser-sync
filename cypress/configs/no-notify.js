8731b989d2c476d83f449c7280ad24a81ff0a97a
Shane Osbourne <sosbourne@duckduckgo.com>
2023-09-09 09:53:33 +0100

    removing cypress

--- cypress/configs/no-notify.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    server: 'packages/browser-sync/test/fixtures',
-    open: false,
-    watch: true,
-    online: false,
-    logLevel: 'silent',
-    minify: false,
-    notify: false,
-};
